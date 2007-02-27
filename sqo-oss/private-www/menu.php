04e28c0f3ae46fb59ba1a44e99a42519c5e4aa1b
Kostas Stroggylos <circular@aueb.gr>
2007-02-27 10:39:52 +0000

    Cleaned up the initial prototype items

--- sqo-oss/private-www/menu.php
+++
@@ -1,8 +0,0 @@
-<div class="leftmenutitle">SQO-OSS</div>
-<div class="leftmenu">
-	<a href="mailing_lists.php">Mailing lists archives</a><br/>
-	<a href="password.php">Change password</a><br/>
-	<a href="repository.php">Browse SQO-OSS repository</a><br/>
-	<b>External Links</b><br/>	
-	<a href="horde">Tasks / Calendar / Contacts</a><br/>
-</div>
