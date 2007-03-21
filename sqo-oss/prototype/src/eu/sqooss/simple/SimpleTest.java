b6bcf0333d0b3af3eb95deb8702b8f3bd38da302
Kostas Stroggylos <circular@aueb.gr>
2007-03-21 11:39:49 +0000

    Added initial version of a simple tool to check the functionality implemented so far

---
+++ sqo-oss/prototype/src/eu/sqooss/simple/SimpleTest.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package eu.sqooss.simple;
+
+import java.util.Vector;
+
+/**
+ * @author circular
+ *
+ */
+public class SimpleTest {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+    private void storeProjectInfo() {
+        //url, name, website, contactPoint, srcPath, mailPath
+    }
+    
+    private void checkOut(long revision) {
+        
+    }
+    
+    private void storeProjectFiles(Vector<String> files) {
+        
+    }
+    
+    private double runWCTool(String file, long revision) {
+        return 0.0;
+    }
+    
+    private void storeMetrics(int projectID, long revision, String file) {
+        //run CW tool, get the metric value, store it in db
+    }
+}
