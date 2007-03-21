3879bd744e44b8b0f104939be0eea3f6c0b26105
Dimitris Mitropoulos <dimitro@aueb.gr>
2007-03-21 13:54:36 +0000

    finished with runWCTool. Added SimpleTest constructor

--- sqo-oss/prototype/src/eu/sqooss/simple/SimpleTest.java
+++ sqo-oss/prototype/src/eu/sqooss/simple/SimpleTest.java
@@ -3,11 +3,17 @@
  */
 package eu.sqooss.simple;
 
-import java.io.BufferedInputStream;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Vector;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import eu.sqooss.vcs.RepositoryFactory;
+import eu.sqooss.vcs.RepositoryType;
+import eu.sqooss.vcs.SvnRepository;
 
 /**
  * @author circular
@@ -18,9 +24,25 @@ public class SimpleTest {
     /**
      * @param args
      */
+	private SvnRepository repository;
+	private RepositoryType type;
+	private String serverPath;
+	private String localPath;
+	private String username;
+	private String password;
+	
+	
+	public SimpleTest() throws Exception {
+		serverPath = "https://svn.sqo-oss.eu/";
+    	localPath = "./svntest";
+    	username = "svnviewer";
+    	password = "Sq0V13weR";
+    	type = RepositoryType.SVN;
+    	repository = (SvnRepository)RepositoryFactory.getRepository(localPath, serverPath, username, password, type);
+	}
+	
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        // TODO Auto-generated method stub 	
     }
 
     private void storeProjectInfo() {
@@ -28,7 +50,7 @@ public class SimpleTest {
     }
     
     private void checkOut(long revision) {
-        
+    	
     }
     
     private void storeProjectFiles(Vector<String> files) {
@@ -48,7 +70,8 @@ public class SimpleTest {
         try {
             p = Runtime.getRuntime().exec(args);
             if(p != null) {
-                BufferedReader b = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                BufferedReader b = 
+                	new BufferedReader(new InputStreamReader(p.getInputStream()));
                 output = b.readLine();
                 b.close();
             }
@@ -56,9 +79,16 @@ public class SimpleTest {
             return 0.0;
         }
         
-        //parse the output
+        Pattern pattern = Pattern.compile("[0-9]+");
+        Matcher matcher = pattern.matcher(output);
+        if (matcher.find()) {
+        	result = new Double(matcher.group(0));
+        } else {
+        	result = 0.0;
+        }
         
         return result;
+        
     }
     
     private void storeMetrics(int projectID, long revision, String file) {
