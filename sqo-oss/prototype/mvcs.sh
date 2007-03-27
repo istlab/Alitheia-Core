c8dc4241bed0cce36800f656e854ba2ee7aed46b
Vassilios Karakoidas <bkarak@aueb.gr>
2007-03-27 14:07:08 +0000

    Removed mvcs.sh script from the root directory

--- sqo-oss/prototype/mvcs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-
-#Construct the program's classpath
-CP=`find lib -type f -name '*.jar'|while read file; do echo $file; done | tr '\n' ':'`
-
-java -classpath $CP:$CLASSPATH:bin eu.sqooss.vcs.CmdLine $@
