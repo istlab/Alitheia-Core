19e28a0bacfed8dffd07495c8d918768edf4e3bf
Vassilios Karakoidas <bkarak@aueb.gr>
2007-03-29 15:51:52 +0000

    Added parser configuration class for the sqooss.properties file (use Configuration.getInstance())

---
+++ sqo-oss/prototype/src/eu/sqooss/tool/ConfigurationOptions.java
@@ -0,0 +1,12 @@
+package eu.sqooss.tool;
+
+/**
+ * Available options in sqooss.properties
+ * Use them with the configurator class
+ */
+public enum ConfigurationOptions {
+    DB_FILE,
+    VCS_SPOOL,
+    PLUGINS,
+    SQOOSS_HOME
+}
---
+++ sqo-oss/prototype/src/eu/sqooss/tool/Configurator.java
@@ -0,0 +1,55 @@
+package eu.sqooss.tool;
+
+import java.io.FileInputStream;
+
+import java.util.Properties;
+
+/**
+ * The Configurator class holds the configuration
+ * information stored in the sqooss.properties
+ * 
+ */
+public class Configurator {
+    private final static Configurator defaultInstance;
+    //
+    Properties configuration;
+    String sqoossHome;
+    
+    static {
+        defaultInstance = new Configurator();
+    }
+    
+    private Configurator() {
+        super();
+        sqoossHome = System.getenv("SQOOSS_HOME");
+        configuration = new Properties();
+        try {
+            FileInputStream fis = new FileInputStream(sqoossHome + "/sqooss.properties");
+            configuration.load(fis);
+            fis.close();
+        } catch (Exception e) {
+            return;
+        }
+    }
+    
+    public String getValue(ConfigurationOptions option) {
+        if(option.compareTo(ConfigurationOptions.DB_FILE) == 0) {
+            return configuration.getProperty("DB_FILE");
+        }
+        if(option.compareTo(ConfigurationOptions.PLUGINS) == 0) {
+            return configuration.getProperty("PLUGINS");
+        }
+        if(option.compareTo(ConfigurationOptions.SQOOSS_HOME) == 0) {
+            return sqoossHome;
+        }        
+        if(option.compareTo(ConfigurationOptions.VCS_SPOOL) == 0) {
+            return configuration.getProperty("VCS_SPOOL");
+        }
+        
+        return null;
+    }
+    
+    public static Configurator getInstance() {
+        return defaultInstance;
+    }
+}
