918b6d98cd894795d2b73d0428dbb1d41aeee507
Dimitris Mitropoulos <dimitro@aueb.gr>
2007-03-29 10:45:20 +0000

    removed CVS (not supported)

--- sqo-oss/prototype/src/eu/sqooss/vcs/ProjectType.java
+++ sqo-oss/prototype/src/eu/sqooss/vcs/ProjectType.java
@@ -31,12 +31,11 @@
 package eu.sqooss.vcs;
 
 /**
- * @author circular
+ * @author circular, dimitro
  *
  * Represents the various types of projects supported.
  */
 public enum ProjectType {
-		CVS,
 		SVN,
 		Url, //ftp or www location of the latest version of a project
 		Local //a project that is synced via an external interface and we analyze locally
