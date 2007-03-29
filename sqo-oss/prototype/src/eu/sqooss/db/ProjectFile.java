d3eae5722e90ce297e0e505f9c4a9b9994933c14
Vassilios Karakoidas <bkarak@aueb.gr>
2007-03-29 10:34:41 +0000

    Added setters and getters for status attribute

--- sqo-oss/prototype/src/eu/sqooss/db/ProjectFile.java
+++ sqo-oss/prototype/src/eu/sqooss/db/ProjectFile.java
@@ -65,4 +65,12 @@ public class ProjectFile {
     public void setName(String name) {
 	this.name = name;
     }
+    
+    public String getStatus() {
+	return status;
+    }
+    
+    public void setStatus(String status) {
+	this.status = status;
+    }
 }
