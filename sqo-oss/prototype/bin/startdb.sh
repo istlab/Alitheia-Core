e5490608f4389a2025e6afa187b37e941c0ddceb
Vassilios Karakoidas <bkarak@aueb.gr>
2007-04-04 18:22:27 +0000

    Made startup scripts aware of the SQOOSS_HOME environment variable (see INSTALL)

--- sqo-oss/prototype/bin/startdb.sh
+++ sqo-oss/prototype/bin/startdb.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-SQOOSS_HOME=".."
-
 java -cp $SQOOSS_HOME/lib/hsqldb.jar org.hsqldb.Server -database.0 $SQOOSS_HOME/data/sqoossdb -dbname.0 sqooss
--- sqo-oss/prototype/bin/tool.sh
+++ sqo-oss/prototype/bin/tool.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-#Construct the program's classpath
-CP=`find lib -type f -name '*.jar'|while read file; do echo $file; done | tr '\n' ':'`
-SQOOSS_JAR="dist/sqo-oss.jar"
+# Construct the program's classpath
+# CP=`find lib -type f -name '*.jar'|while read file; do echo $file; done | tr '\n' ':'`
+
+LIB="$SQOOSS_HOME/lib"
+CP="$LIB/asm.jar:$LIB/c3p0-0.9.1.jar:$LIB/cg$LIB-2.1.3.jar:$LIB/commons-cli.jar:$LIB/commons-collections-2.1.1.jar:$LIB/commons-logging.jar:$LIB/dom4j-1.6.1.jar:$LIB/hibernate-tools.jar:$LIB/hibernate3.jar:$LIB/hsqldb.jar:$LIB/jta.jar:$LIB/junit-3.8.1.jar:$LIB/svnkit.jar"
+SQOOSS_JAR="$SQOOSS_HOME/dist/sqo-oss.jar"
 
 java -classpath $SQOOSS_JAR:$CP:$CLASSPATH eu.sqooss.tool.Main $@
