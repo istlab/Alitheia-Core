44b1f9db90bf0b3111359138b16e6da06dfaa14f
Vassilios Karakoidas <bkarak@aueb.gr>
2007-04-04 08:48:12 +0000

    Implemented plugin list option

--- sqo-oss/prototype/src/eu/sqooss/tool/PluginCLI.java
+++ sqo-oss/prototype/src/eu/sqooss/tool/PluginCLI.java
@@ -2,6 +2,11 @@ package eu.sqooss.tool;
 
 import org.apache.commons.cli.*;
 
+import eu.sqooss.plugin.PluginList;
+import eu.sqooss.plugin.Plugin;
+
+import eu.sqooss.util.ReadOnlyIterator;
+
 /**
  * Command Line handling class for the plugin options
  */
@@ -9,10 +14,23 @@ public class PluginCLI extends CLI {
     
     private PluginCLI(String[] args) {
         super(args);
+        
+        options.addOption("l","list",false,"List available plugins");
     }    
     
     public static void parse(String[] args) {
         PluginCLI pcli = new PluginCLI(args);
         CommandLine cmdLine = pcli.parseArgs();
+        
+        if(cmdLine.hasOption("list")) {
+                System.out.println("Print all available Modules:\n");
+                PluginList pl = PluginList.getInstance();
+                ReadOnlyIterator roi = pl.getPlugins();
+                
+                while(roi.hasNext()) {
+                System.out.println(((Plugin)roi.next()).toString());
+            } 
+            return;
+        }
     }
 }
