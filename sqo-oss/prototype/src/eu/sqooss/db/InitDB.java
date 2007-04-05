56f2da1aa6f6df466c50324cb510bbabfd96432c
Vassilios Karakoidas <bkarak@aueb.gr>
2007-04-05 09:08:19 +0000

    Added default metricType records

--- sqo-oss/prototype/src/eu/sqooss/db/InitDB.java
+++ sqo-oss/prototype/src/eu/sqooss/db/InitDB.java
@@ -1,11 +1,40 @@
 package eu.sqooss.db;
 
+import org.hibernate.Session;
+
+import eu.sqooss.util.HibernateUtil;
+
 /**
  * Initializes the database with the default values
  * (invoke only once)
  */
 public class InitDB {
+    private static MetricType addMetricType(String type) {
+        MetricType mt = new MetricType();
+        
+        mt.setType(type);
+        
+        return mt;
+    }
+    
     public static void main(String[] args) {
-        // TODO write the values here
+        Session session =
+            HibernateUtil.getSessionFactory().getCurrentSession();
+        
+        
+        session.beginTransaction();
+        
+        // add metric_type
+        session.save(addMetricType("Code Metric"));
+        session.save(addMetricType("Statistical measure"));
+        
+        // add plugins
+        
+        // add metrics
+        
+        session.getTransaction().commit();
+        
+        // close the session
+        HibernateUtil.getSessionFactory().close();
     }
 }
