c735cbe46495fbc90630121958ac9060a5a7c708
Georgios Gousios <gousiosg@aueb.gr>
2007-04-05 16:36:01 +0000

    Method to add a project entry to the database

--- sqo-oss/prototype/src/eu/sqooss/tool/ProjectsCLI.java
+++ sqo-oss/prototype/src/eu/sqooss/tool/ProjectsCLI.java
@@ -31,7 +31,18 @@
 
 package eu.sqooss.tool;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.apache.commons.cli.CommandLine;
+import org.hibernate.Session;
+
+import eu.sqooss.db.MetricType;
+import eu.sqooss.db.StoredProject;
+import eu.sqooss.util.HibernateUtil;
+import eu.sqooss.vcs.InvalidRepositoryException;
+import eu.sqooss.vcs.Repository;
+import eu.sqooss.vcs.RepositoryFactory;
 
 /**
  * Handling of command line for and dispatching of project-related
@@ -39,6 +50,13 @@ import org.apache.commons.cli.CommandLine;
  */
 public class ProjectsCLI extends CLI {
 
+    private static String basepath;
+    
+    static{
+        basepath = Configurator.getInstance().getValue(ConfigurationOptions.SQOOSS_HOME) + 
+            File.separatorChar   + "projects";
+    }
+    
     protected ProjectsCLI(String[] args) {
         super(args);
         /*Functions*/
@@ -55,16 +73,17 @@ public class ProjectsCLI extends CLI {
                 "Delete a project version");
 
         /*Arguments to functions*/
-        options.addOption("i", "project-id", false, "Project ID");
-        options.addOption("n", "project-name", false, "Project Name");
-        options.addOption("l", "project-local-path", false,
+        options.addOption("i", "project-id", true, "Project ID");
+        options.addOption("n", "project-name", true, "Project Name");
+        options.addOption("l", "project-local-path", true,
                 "Project local path");
-        options.addOption("c", "project-remote-part", false,
+        options.addOption("r", "project-remote-path", true,
                 "Project remote path");
-        options.addOption("s", "project-svn-url", false, "Project SVN URL");
+        options.addOption("v", "version", true, "Project version");
+        options.addOption("s", "project-svn-url", true, "Project SVN URL");
 
         /*Help*/
-        options.addOption("h", "help", false, "Project functions and options");
+        options.addOption("h", "help", true, "Project functions and options");
     }
 
     public void parse() {
@@ -80,14 +99,95 @@ public class ProjectsCLI extends CLI {
         }
         
         if(cmdLine.hasOption("ap")) {
-            if (!ensureOptions(cmdLine, "n l c s")) 
-                error("Missing options", cmdLine);
+            if (!ensureOptions(cmdLine, "n r s")) 
+                error("One of the required options (n,r,s) is missing " +
+                        "or has no argument", cmdLine);
+            
+            String name = cmdLine.getOptionValue("n").trim();
+            String remotePath = cmdLine.getOptionValue("r").trim();
+            String url = cmdLine.getOptionValue("s").trim();
+            String localPath = cmdLine.getOptionValue("l");
+            
+            assert name != null && name != "";
+            assert remotePath != null && remotePath != "";
+            assert url != null && url != "";
+            
+            //if(!checkProjectExists(name, localPath, remotePath, url))
+            addProject(name, remotePath, localPath, url);
+        }
+        
+        if(cmdLine.hasOption("av")) {
+            if (!ensureOptions(cmdLine, "i v")) 
+                error("One of the required options (i) is missing", cmdLine);
         }
         
         if(cmdLine.hasOption("lp")) {
             
         }
+        
+        if(cmdLine.hasOption("lv")) {
+            if (!ensureOptions(cmdLine, "i")) 
+                error("One of the required options (i) is missing", cmdLine);
+        }
     }
     
+    /**
+     * Adds a project to the StoredProject table and checks out 
+     * its current version to the base path 
+     */
+    
+    private void addProject(String name, String remotePath, String localPath, String url) {
+        
+        String projectPath = null;
+        
+        if(localPath != null)
+            projectPath = (localPath == basepath + File.separatorChar + name)?localPath:localPath;
+        else
+            projectPath = basepath + File.separatorChar + name;
+        
+        System.out.println(basepath);
+        System.out.println(projectPath);
+        
+        File f = new File(projectPath);
+        
+        if(f.exists()) {
+            error("Project directory already exists - not adding");
+        }
+        else {
+          log("Creating project dir: " + f.getAbsolutePath());
+          f.mkdirs();
+        }
+        
+        Repository r = null;
+        
+        try {
+            r = RepositoryFactory.getRepository(projectPath, url);
+        } catch (InvalidRepositoryException e) {
+            
+        }
+        
+        long curver = r.getCurrentVersion(false);
+        log("Current project version:" + curver);
+        
+        f = new File(projectPath); 
+       
+        
+        log("Adding project entry to the database");
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        
+        session.beginTransaction();
+        
+        StoredProject p = new StoredProject();
+        p.setName(name);
+        p.setLocalPath(projectPath);
+        p.setRemotePath(remotePath.trim());
+        p.setSvnUrl(url);
+        p.setContactPoint("admin@");
+        p.setWebsite(url);
+        p.setMailPath(projectPath + File.separatorChar + "mail");
+        
+        session.save(p);
 
+        session.getTransaction().commit();
+    }
 }
