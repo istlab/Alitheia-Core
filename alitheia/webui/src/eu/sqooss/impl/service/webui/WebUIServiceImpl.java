9062ad49df8b1583f3f304f8836f0bcad0f258ab
Adriaan de Groot <groot@kde.org>
2007-09-24 16:15:18 +0000

    r3260@admins-computer:  adridg | 2007-09-24 17:47:10 +0200  Implementation for webui

---
+++ alitheia/webui/src/eu/sqooss/impl/service/webui/WebUIServiceImpl.java
@@ -0,0 +1,20 @@
+package eu.sqooss.impl.service.webui;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Properties;
+import java.util.Vector;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
+
+import eu.sqooss.services.webui.WebUIService;
+
+public class WebUIServiceImpl implements WebUIService {
+}
+
+
