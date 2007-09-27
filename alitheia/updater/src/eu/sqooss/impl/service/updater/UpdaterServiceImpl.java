b4649955dbb9156ea288e78481dd7228bae214aa
Georgios Gousios <gousiosg@aueb.gr>
2007-09-27 10:09:10 +0000

    The log service is exported by the LogManager class, not the logger interface...

--- alitheia/updater/src/eu/sqooss/impl/service/updater/UpdaterServiceImpl.java
+++ alitheia/updater/src/eu/sqooss/impl/service/updater/UpdaterServiceImpl.java
@@ -1,34 +1,24 @@
 /*
-This file is part of the Alitheia system, developed by the SQO-OSS
-consortium as part of the IST FP6 SQO-OSS project, number 033331.
-
-Copyright 2007 by the SQO-OSS consortium members <info@sqo-oss.eu>
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are
-met:
-
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-
-    * Redistributions in binary form must reproduce the above
-      copyright notice, this list of conditions and the following
-      disclaimer in the documentation and/or other materials provided
-      with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-*/
+ * This file is part of the Alitheia system, developed by the SQO-OSS consortium
+ * as part of the IST FP6 SQO-OSS project, number 033331. Copyright 2007 by the
+ * SQO-OSS consortium members <info@sqo-oss.eu> Redistribution and use in source
+ * and binary forms, with or without modification, are permitted provided that
+ * the following conditions are met: Redistributions of source code must retain
+ * the above copyright notice, this list of conditions and the following
+ * disclaimer. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution. THIS
+ * SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 package eu.sqooss.impl.service.updater;
 
@@ -48,43 +38,57 @@ import org.osgi.service.http.NamespaceException;
 
 import eu.sqooss.services.updater.UpdaterService;
 import eu.sqooss.service.logging.Logger;
+import eu.sqooss.service.logging.LogManager;
 
 public class UpdaterServiceImpl extends HttpServlet implements UpdaterService {
 
-    private ServiceReference serviceRef = null;    
-    private HttpService httpService = null;    
-    private Logger logService = null;
-    
-    public UpdaterServiceImpl(BundleContext bc) throws ServletException, NamespaceException {
-        
-        logService = (Logger) bc.getServiceReference("eu.sqooss.service.logging.Logger");
-        
+    private ServiceReference serviceRef = null;
+
+    private HttpService httpService = null;
+
+    private LogManager logService = null;
+
+    private Logger logger = null;
+
+    public UpdaterServiceImpl(BundleContext bc) throws ServletException,
+            NamespaceException {
+
+        logService = (LogManager) bc.getServiceReference("eu.sqooss.service.logging.LogManager");
+
         if (logService != null) {
-            logService.setConfigurationProperty("file.name", "update-service.log");
-            logService.setConfigurationProperty("message.format", "text/plain");
+            logger = logService.createLogger("eu.sqooss.updater");
+            if (logger != null) {
+                logger.setConfigurationProperty("file.name","update-service.log");
+                logger.setConfigurationProperty("message.format", "text/plain");
+                System.out.println("Got logging!");
+            }
+            System.out.println("Got log service but not a logger");
         }
-        System.out.println("Got logging!");
+
         serviceRef = bc.getServiceReference("org.osgi.service.http.HttpService");
+        
         if (serviceRef != null) {
             httpService = (HttpService) bc.getService(serviceRef);
-            httpService.registerServlet("/updater", (Servlet) this, new Hashtable(), null);
+            httpService.registerServlet("/updater", (Servlet) this,
+                    new Hashtable(), null);
         } else {
-            logService.severe("Could not load the HTTP service."); 
+            logger.severe("Could not load the HTTP service.");
         }
     }
-    
+
     public void update(String path) {
-        logService.info("Request to update path:" + path);
+        logger.info("Request to update path:" + path);
     }
-    
-    public void doGet(HttpServletRequest request, HttpServletResponse response) 
-        throws ServletException,IOException {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         String s = request.getParameter("path");
-        if ( s != null )
+        if (s != null)
             update(s);
         else {
             response.setContentType("text/html");
-            response.getWriter().write("<html><head><title>Error</title></head><body><h1>Error</h1></body></html>");
+            response.getWriter().write("<html><head><title>Error</title>" +
+            		"</head><body><h1>Error</h1></body></html>");
         }
     }
 
