b44cbb3d9947deef59a72b4343dc4163dc6e6bac
Adriaan de Groot <groot@kde.org>
2007-10-11 20:41:56 +0000

    Actually go and create the STORED_PROJECT table with whatever driver we end up with. This demonstrates that derby will write a whole metric crapload of files in equinox/derbyDB; this is expected.
    At this point, Paul can take over with his SQL-fu and additional knowledge; the monkey-work of getting a database driver in there at all has been done, and we can talk about the API of the DB service and how it's going to do pooling and whatnot.

--- alitheia/db/src/eu/sqooss/impl/service/db/DBServiceImpl.java
+++ alitheia/db/src/eu/sqooss/impl/service/db/DBServiceImpl.java
@@ -1,21 +1,21 @@
 /*
  * This file is part of the Alitheia system, developed by the SQO-OSS
  * consortium as part of the IST FP6 SQO-OSS project, number 033331.
- * 
+ *
  * Copyright 2007 by the SQO-OSS consortium members <info@sqo-oss.eu>
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
  * met:
- * 
+ *
  *     * Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
- * 
+ *
  *     * Redistributions in binary form must reproduce the above
  *       copyright notice, this list of conditions and the following
  *       disclaimer in the documentation and/or other materials provided
  *       with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -27,10 +27,16 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  */
 
 package eu.sqooss.impl.service.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
@@ -41,8 +47,11 @@ import eu.sqooss.service.logging.LogManager;
 import eu.sqooss.impl.service.logging.LogManagerConstants;
 
 public class DBServiceImpl {
-    private Logger logger;
-    
+    private Logger logger = null;
+    // This is the database connection; we may want to do more pooling here.
+    private Connection dbConnection = null;
+    private Statement dbStatement = null;
+
     /**
      * Attempt to get the Postgres JDBC connector and initialize
      * a connection to the Postgres instance (just to check that
@@ -53,7 +62,7 @@ public class DBServiceImpl {
     private boolean getPostgresJDBC() {
         return false;
     }
-    
+
     /**
      * Attempt to get the Derby JDBC connector and initialize
      * a connection to the Derby instance -- this is intended
@@ -62,9 +71,33 @@ public class DBServiceImpl {
      * @return @c true on success
      */
     private boolean getDerbyJDBC() {
-        return false;
+        String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+
+        try {
+            Class.forName(driver).newInstance();
+            logger.info("Created Derby JDBC instance.");
+            dbConnection = DriverManager.getConnection(
+                "jdbc:derby:derbyDB;create=true", null);
+            logger.info("Created Derby connection.");
+            dbConnection.setAutoCommit(false);
+            dbStatement = dbConnection.createStatement();
+        } catch (InstantiationException e) {
+            logger.warning("Could not instantiate Derby JDBC connection.");
+            return false;
+        } catch (ClassNotFoundException e) {
+            logger.warning("Could not get class for Derby JDBC.");
+            return false;
+        } catch (IllegalAccessException e) {
+            logger.warning("SEGV. Core dumped in Derby.");
+            return false;
+        } catch (SQLException e) {
+            logger.warning("SQL Exception while instantiating Derby.");
+            return false;
+        }
+
+        return true;
     }
-    
+
     public DBServiceImpl() {
         logger = LogManager.getInstance().createLogger("sqooss.database");
         if (logger != null) {
@@ -72,10 +105,19 @@ public class DBServiceImpl {
         } else {
             System.out.println("# DB service failed to get logger.");
         }
-    
+
         if (!getPostgresJDBC()) {
             if (!getDerbyJDBC()) {
                 logger.severe("DB service got no JDBC connectors.");
+            }
+        }
+
+        if (dbStatement != null) {
+            try {
+                dbStatement.execute("create table STORED_PROJECT (ID int, NAME varchar(80))");
+                logger.info("Created table STORED_PROJECT.");
+            } catch (SQLException e) {
+                logger.warning("SQL Exception while creating table.");
             }
         }
     }
