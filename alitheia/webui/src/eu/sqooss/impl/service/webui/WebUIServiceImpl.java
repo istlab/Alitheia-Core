4c87cee629ab1a9b2cd5539fcd28fb6bb37e0d0a
Adriaan de Groot <groot@kde.org>
2007-10-18 15:13:38 +0000

    Resurrect the WebUI part

---
+++ alitheia/webui/src/eu/sqooss/impl/service/webui/WebUIServiceImpl.java
@@ -0,0 +1,87 @@
+/*
+ * This file is part of the Alitheia system, developed by the SQO-OSS
+ * consortium as part of the IST FP6 SQO-OSS project, number 033331.
+ *
+ * Copyright 2007 by the SQO-OSS consortium members <info@sqo-oss.eu>
+ * Copyright 2007 by Adriaan de Groot <groot@kde.org>
+ *
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package eu.sqooss.impl.service.webui;
+
+import java.util.Hashtable;
+
+import javax.servlet.Servlet;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.http.HttpService;
+
+import eu.sqooss.service.logger.Logger;
+import eu.sqooss.service.logger.LogManager;
+import eu.sqooss.service.webui.WebUIService;
+
+public class WebUIServiceImpl implements WebUIService {
+    ServiceReference serviceref = null;
+    HttpService httpservice = null;
+    WebUIServer httpui = null;
+
+    public WebUIServiceImpl(BundleContext bc) throws Exception {
+        ServiceReference logServiceRef = bc.getServiceReference("eu.sqooss.service.logging.LogManager");
+        logService = (LogManager) bc.getService(logServiceRef);
+        logger = logService.createLogger(Logger.NAME_SQOOSS_WEBUI);
+        if (logger != null) {
+            logger.info("WebUI service created.");
+        } else {
+            System.out.println("# WebUI failed to get logger.");
+        }
+
+        serviceref = bc.getServiceReference("org.osgi.service.http.HttpService");
+        if (serviceref != null) {
+            httpservice = (HttpService) bc.getService(serviceref);
+            httpui = new WebUIServer(bc);
+            httpservice.registerServlet("/", (Servlet) httpui,
+                                        new Hashtable(), null);
+        } else {
+            System.out.println("! Could not load the HTTP service.");
+        }
+    }
+
+    public String[] getConfigurationKeys() {
+        return null;
+    }
+
+    public String getConfigurationProperty(String key) {
+        return key;
+    }
+
+    public void setConfigurationProperty(String key, String val) {
+    }
+}
+
+
