5f373e19b3cf102b270895e3422c7570c0c240e7
Adriaan de Groot <groot@kde.org>
2007-10-21 12:41:05 +0000

    Make a stub implementation of the FDS. Make it possible to run make cleanly from the alitheia/ dir by setting PREFIX if it is unset.

---
+++ alitheia/fds/src/eu/sqooss/impl/service/fds/FDSServiceImpl.java
@@ -0,0 +1,59 @@
+/*
+ * This file is part of the Alitheia system, developed by the SQO-OSS
+ * consortium as part of the IST FP6 SQO-OSS project, number 033331.
+ *
+ * Copyright 2007 by the SQO-OSS consortium members <info@sqo-oss.eu>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package eu.sqooss.impl.service.fds;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
+import eu.sqooss.service.logging.LogManager;
+import eu.sqooss.service.logging.Logger;
+
+public class FDSServiceImpl {
+    private LogManager logService = null;
+    private Logger logger = null;
+
+    public FDSServiceImpl(BundleContext bc) {
+        ServiceReference serviceRef = bc.getServiceReference("eu.sqooss.service.logging.LogManager");
+        logService = (LogManager) bc.getService(serviceRef);
+        logger = logService.createLogger(Logger.NAME_SQOOSS_TDS);
+        if (logger != null) {
+            logger.info("FDS service created.");
+        } else {
+            System.out.println("# FDS failed to get logger.");
+        }
+    }
+}
+
+
+// vi: ai nosi sw=4 ts=4 expandtab
+
