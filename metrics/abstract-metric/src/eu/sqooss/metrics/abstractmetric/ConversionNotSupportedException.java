adfc1a40b6c093c5062ce3d20f53b766e34d4437
Adriaan de Groot <groot@kde.org>
2007-10-27 17:05:40 +0000

    Ignores; warnings--

--- metrics/abstract-metric/src/eu/sqooss/metrics/abstractmetric/ConversionNotSupportedException.java
+++ metrics/abstract-metric/src/eu/sqooss/metrics/abstractmetric/ConversionNotSupportedException.java
@@ -1,6 +1,7 @@
 package eu.sqooss.metrics.abstractmetric;
 
 public class ConversionNotSupportedException extends Exception {
+    private static final long serialVersionUID = 1;
     public ConversionNotSupportedException(String msg) {
         super(msg);
     }
