9d04b5d5ee1909f0dbea2b8b0f5300c0a03dad3c
Adriaan de Groot <groot@kde.org>
2007-11-06 09:58:27 +0000

    Spaces, but mostly that $Revision won't be expanded unless you go through and add svn keyword expansions (which are generally considered a bad idea).

--- metrics/productivity-metric/src/eu/sqooss/impl/metrics/productivity/ProductivityBase.java
+++ metrics/productivity-metric/src/eu/sqooss/impl/metrics/productivity/ProductivityBase.java
@@ -1,23 +1,23 @@
 /*
  * This file is part of the Alitheia system, developed by the SQO-OSS
  * consortium as part of the IST FP6 SQO-OSS project, number 033331.
- * 
+ *
  * Copyright 2007 by the SQO-OSS consortium members <info@sqo-oss.eu>
  * Copyright 2007 Georgios Gousios <gousiosg@gmail.com>
- * 
- * 
+ *
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
  * met:
- * 
+ *
  *     * Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
- * 
+ *
  *     * Redistributions in binary form must reproduce the above
  *       copyright notice, this list of conditions and the following
  *       disclaimer in the documentation and/or other materials provided
  *       with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -29,7 +29,7 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  */
 
 package eu.sqooss.impl.metrics.productivity;
@@ -46,20 +46,20 @@ import eu.sqooss.service.db.StoredProject;
 import eu.sqooss.service.logging.Logger;
 
 public class ProductivityBase implements ProductivityMetric {
-  
+
     protected TDSService service;
     protected SCMAccessor svn;
     protected Logger log;
-    private String revision = "$Revision: ";
-    
+    private String revision = "Spanky Monkey:17";
+
     protected ProductivityBase() {
-        
-	//FIXME: Dummy getAccessor value to fix the build 
+
+	//FIXME: Dummy getAccessor value to fix the build
 	svn = (SCMAccessor) service.getAccessor(1);
     }
-    
+
     protected void run() {
-        
+
     }
 
     public String getAuthor() {
@@ -94,14 +94,14 @@ public class ProductivityBase implements ProductivityMetric {
     public boolean update() {
         return false;
     }
-    
+
     public boolean run (ProjectVersion a) {
-        return false;   
+        return false;
     }
-    
+
     public MetricResult getResult(ProjectVersion a) {
         return null;
-        
+
     }
 
     public boolean delete(StoredProject a) {
