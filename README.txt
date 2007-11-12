7a09a8f691d0ddc07fcb4140f554876101b22be5
Adriaan de Groot <groot@kde.org>
2007-11-12 14:14:03 +0000

    r4685@admins-computer:  adridg | 2007-11-12 15:13:49 +0100  Add documentation on runtime (startup-time) testing

--- README.txt
+++ README.txt
@@ -64,6 +64,38 @@ key=value pairs to define properties. The supported properties are
 described in the file itself.
 
 ***
+*** Running tests
+***
+
+Alitheia contains a run-time testing facility. The bundle tester
+(installed by default) will run, on startup, the selfTest() method
+defined in each service, but only if the startup tests are enabled
+in the runtime configuration file. To enable them, set
+
+eu.sqooss.tester.enable=YES
+
+(case sensitive, must be exactly "YES"). Then each service object
+registered with the OSGi framework (on startup of the tester bundle)
+will be examined. Those with a selfTest() method will be called.
+
+The selfTest() method should return null on success and an Object
+which describes the failure (for instance, a String) on failure.
+Failures are logged. Here is a sample:
+
+	public Object selfTest() {
+		// Normal test functions return true on success
+		if (actualtestFunction()) {
+			return null;
+		} else {
+			return new String("My actualTestFunction failed.");
+		}
+	}
+
+This selfTest() method must be defined in the object which is passed
+to BundleContext.registerService().
+
+
+***
 *** Alternate logging subsystem
 ***
 
---
+++ alitheia/webadmin/src/eu/sqooss/impl/service/webadmin/WebUIServer.java
@@ -0,0 +1,337 @@
+/*
+ * This file is part of the Alitheia system, developed by the SQO-OSS
+ * consortium as part of the IST FP6 SQO-OSS project, number 033331.
+ *
+ * Copyright 2007 by the SQO-OSS consortium members <info@sqo-oss.eu>
+ * Copyright 2007 by Adriaan de Groot <groot@kde.org>
+ *
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package eu.sqooss.impl.service.webui;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
+import eu.sqooss.service.logging.LogManager;
+import eu.sqooss.service.logging.Logger;
+import eu.sqooss.service.db.DBService;
+import eu.sqooss.util.SQOUtils;
+
+import java.util.Hashtable;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+// Java Extensions
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServlet;
+
+public class WebUIServer extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    private BundleContext bundlecontext = null;
+
+    private LogManager logService = null;
+    private Logger logger = null;
+    private DBService dbService = null;
+    private Hashtable<String,String[]> staticContentMap;
+    private Hashtable<String,String> dynamicContentMap;
+    private Hashtable<String,String> dynamicSubstitutions;
+
+    private void getLogger(BundleContext bc) {
+        ServiceReference serviceRef = null;
+        serviceRef = bc.getServiceReference(LogManager.class.getName());
+        logService = (LogManager) bc.getService(serviceRef);
+
+        if (logService != null) {
+            logger = logService.createLogger(Logger.NAME_SQOOSS_WEBUI);
+
+            if (logger != null) {
+                logger.info("WebAdmin got logger.");
+            }
+        } else {
+            System.out.println("! Got neither a service nor a logger.");
+        }
+    }
+
+    private void getDB(BundleContext bc) {
+        ServiceReference serviceRef = bc.getServiceReference(DBService.class.getName());
+        if (serviceRef != null) {
+            dbService = (DBService) bc.getService(serviceRef);
+            if (logger != null) {
+                logger.info("Got DB service.");
+            }
+        } else {
+            if (logger != null) {
+                logger.warning("Did not get DB service.");
+            } else {
+                System.out.println("! Got no DB service.");
+            }
+        }
+    }
+
+    public WebUIServer(BundleContext bc) {
+        bundlecontext = bc;
+        getLogger(bc);
+        getDB(bc);
+
+
+        staticContentMap = new Hashtable<String,String[]>();
+        String[] flossie = { "image/x-png", "/flossie.png" } ;
+        String[] css = { "text/css", "/alitheia.css" } ;
+        String[] logo = { "image/x-png", "/alitheia.png" } ;
+        staticContentMap.put("logo", flossie);
+        staticContentMap.put("alitheia.css", css);
+        staticContentMap.put("alitheia.png", logo);
+
+        dynamicContentMap = new Hashtable<String,String>();
+        dynamicContentMap.put("about","/about.html");
+        dynamicContentMap.put("status","/index.html");
+        dynamicContentMap.put("index","/index.html");
+        dynamicContentMap.put("addproject","/addproject.html");
+        dynamicContentMap.put("list","/listproject.html");
+
+        dynamicSubstitutions = new Hashtable<String,String>();
+    }
+
+    protected String[] getServiceNames() {
+        if ( bundlecontext != null ) {
+            try {
+                ServiceReference servicerefs[] = bundlecontext.getServiceReferences(
+                    null,null);
+
+                String names[] = new String[servicerefs.length];
+                int i = 0;
+                for (ServiceReference r : servicerefs) {
+                    String s;
+                    Object clazz = r.getProperty( org.osgi.framework.Constants.OBJECTCLASS );
+                    if (clazz != null) {
+                        s = SQOUtils.join( (String[])clazz, ", ");
+                    } else {
+                        s = "No class defined";
+                    }
+                    names[i++]=s;
+                }
+
+                return names;
+            } catch (org.osgi.framework.InvalidSyntaxException e) {
+                logger.severe("Invalid request syntax");
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    protected String[] getBundleNames() {
+        if ( bundlecontext != null ) {
+            Bundle[] bundles = bundlecontext.getBundles();
+            String names[] = new String[bundles.length];
+            int i = 0;
+            String[] statenames = { "uninstalled",
+                "installed",
+                "resolved",
+                "starting",
+                "stopping",
+                "active" } ;
+            String s;
+            for (Bundle b : bundles) {
+                int state = b.getState();
+                s = b.getSymbolicName() + " = " + state + " (" + SQOUtils.bitfieldToString(statenames,state) + ")";;
+                names[i++] = s;
+            }
+            return names;
+        } else {
+            return null;
+        }
+    }
+
+    public String renderList(String[] names) {
+        if ((names != null) && (names.length > 0)) {
+            StringBuilder b = new StringBuilder();
+            for (String s : names) {
+                b.append("<li>" + s + "</li>");
+            }
+            return b.toString();
+        } else {
+            return "<li>&lt;none&gt;</li>";
+        }
+    }
+
+    /**
+    * Sends a resource (stored in the jar file) as a response. The mime-type
+    * is set to @p mimeType . The @p path to the resource should start
+    * with a / .
+    *
+    * Test cases:
+    *   - null mimetype, null path, bad path, relative path, path not found,
+    *   - null response
+    *
+    * TODO: How to simulate conditions that will cause IOException
+    */
+    protected void sendResource(HttpServletResponse response, String mimeType, String path)
+        throws ServletException, IOException {
+        InputStream istream = getClass().getResourceAsStream(path);
+        if ( istream == null ) {
+            // TODO: Is there a more specific exception?
+            throw new IOException( "Path not found: " + path );
+        }
+
+        byte[] buffer = new byte[1024];
+        int bytesRead = 0;
+        int totalBytes = 0;
+
+        if ( logger != null ) {
+            logger.info("Serving " + path + " (" + mimeType + ")");
+        }
+
+        response.setContentType(mimeType);
+        ServletOutputStream ostream = response.getOutputStream();
+        while ( (bytesRead = istream.read(buffer)) > 0 ) {
+            ostream.write(buffer,0,bytesRead);
+            totalBytes += bytesRead;
+        }
+
+        // TODO: Check that the bytes written were as many as the
+        //  file size in the JAR (how? it's an InputStream).
+        if ( logger != null ) {
+            logger.info("Wrote " + totalBytes + " from " + path);
+        }
+    }
+
+    protected void sendTemplate(HttpServletResponse response, String path, Hashtable<String,String> subs)
+        throws ServletException, IOException {
+        BufferedReader istream = new BufferedReader(
+            new InputStreamReader(getClass().getResourceAsStream(path)));
+        if ( istream == null ) {
+            // TODO: Is there a more specific exception?
+            throw new IOException( "Path not found: " + path );
+        }
+
+        if ( logger != null ) {
+            logger.info("Serving template " + path);
+        }
+
+        response.setContentType("text/html");
+        PrintWriter print = response.getWriter();
+        while ( istream.ready() ) {
+            String line = istream.readLine();
+            if ( line.startsWith("@@") && subs.containsKey(line) ) {
+                print.println(subs.get(line));
+            } else {
+                print.println(line);
+            }
+        }
+    }
+
+    private void resetSubstitutions() {
+        dynamicSubstitutions.clear();
+        dynamicSubstitutions.put("@@STATUS","The cruncher is offline.");
+        dynamicSubstitutions.put("@@LOGO","<img src='/logo' id='logo' alt='Logo' />");
+        dynamicSubstitutions.put("@@COPYRIGHT","Copyright 2007 SQO-OSS Consortium members");
+    }
+
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException,
+                                                              IOException {
+        if ( logger != null ) {
+            logger.info("GET path=" + request.getPathInfo());
+        }
+
+        String query = request.getPathInfo();
+        if ( (query != null) && (staticContentMap.containsKey(query)) ) {
+            String[] mapvalues = staticContentMap.get(query);
+            sendResource(response, mapvalues[0], mapvalues[1]);
+        } else {
+            resetSubstitutions();
+            dynamicSubstitutions.put("@@ABOUT","<p class='box'>This is the administrative interface.</p>");
+            if ("list".equals(query)) {
+                dynamicSubstitutions.put("@@PROJECTS",renderList(dbService.listProjects()));
+            } else {
+                dynamicSubstitutions.put("@@BUNDLE",renderList(getBundleNames()));
+                dynamicSubstitutions.put("@@SERVICE",renderList(getServiceNames()));
+            }
+            if ( (query != null) && dynamicContentMap.containsKey(query) ) {
+                sendTemplate(response,dynamicContentMap.get(query),dynamicSubstitutions);
+            } else {
+                sendTemplate(response,"/index.html",dynamicSubstitutions);
+            }
+        }
+    }
+
+    private void addProject(HttpServletRequest request) {
+        resetSubstitutions();
+
+        String name = request.getParameter("name");
+        String website = request.getParameter("website");
+        String contact = request.getParameter("contact");
+        String bts = request.getParameter("bts");
+        String mail = request.getParameter("mail");
+        String scm = request.getParameter("scm");
+
+        if ( (name == null) ||
+            (website == null) ||
+            (contact == null) ||
+            (bts == null) ||
+            (mail == null) ||
+            (scm == null) ) {
+            dynamicSubstitutions.put("@@RESULTS","<p>Add project failed because some of the required information was missing.</p>");
+            return;
+        }
+
+        if (dbService != null) {
+            dbService.addProject(name,website,contact,bts,mail,scm);
+        }
+        logger.info("Added a new project.");
+        dynamicSubstitutions.put("@@RESULTS","<p>New project added successfully.</p>");
+    }
+
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response) throws ServletException,
+                                                               IOException {
+        logger.info("POST path=" + request.getPathInfo());
+        if ("addproject".equals(request.getPathInfo())) {
+            addProject(request);
+            // addProject() has filled in the substitutions by now
+            sendTemplate(response,"/results.html",dynamicSubstitutions);
+        } else {
+            doGet(request,response);
+        }
+    }
+
+}
+
+
