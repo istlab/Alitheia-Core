0779d9780e7608a20a9f0c688d3574cfcbf80f21
Georgios Gousios <gousiosg@aueb.gr>
2007-12-04 16:28:21 +0000

    Implement object serialisation to byte array and serialisation from and to XML

--- metrics/abstract-metric/src/eu/sqooss/metrics/abstractmetric/MetricResultEntry.java
+++ metrics/abstract-metric/src/eu/sqooss/metrics/abstractmetric/MetricResultEntry.java
@@ -31,8 +31,19 @@
  *
  */
 
-
 package eu.sqooss.metrics.abstractmetric;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.commons.codec.binary.Base64;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 /**
  * A metric result entry. Tries to be as generic as possible by storing
@@ -52,23 +63,80 @@ package eu.sqooss.metrics.abstractmetric;
  */
 public class MetricResultEntry {
 
-    byte[] b;
+    private byte[] b;
 
-    public String mimeType;
+    private String mimeType;
 
-    MetricResultEntry(int size) {
-        b = new byte[size];
+    /**
+     * Creates a new MetricResultEntry object
+     * 
+     * @param value - An object encapsulating the value to be returned
+     * @param mime - The mime type of the result value
+     * @throws IOException - When serialization of the passed object fails
+     */
+    public MetricResultEntry(Object value, String mime) throws IOException {
+
+        mimeType = mime;
+
+        ByteArrayOutputStream bout = new ByteArrayOutputStream();
+        ObjectOutputStream oout = new ObjectOutputStream(bout);
+        oout.writeObject(value);
     }
 
     public String toString() {
         return new String(b);
     }
 
-    public String toXML() {
-        return null;
+    public byte[] getByteArray() {
+        return b;
+    }
+    
+    public String getMime() {
+        return mimeType;
     }
 
-    public static MetricResultEntry fromXML(String XML) {
+    public String toXML() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\t\t\t\t<mre>");
+        sb.append("\t\t\t\t<d>");
+        sb.append(Base64.encodeBase64(b));
+        sb.append("\t\t\t\t</d>\n");
+        sb.append("\t\t\t\t<m>");
+        sb.append(mimeType);
+        sb.append("\t\t\t\t</m>\n");
+        sb.append("\t\t\t</mre>\n");
         return null;
+    }
+    
+    /**
+     * Creates a MetricResultEntry object from an XML description. The XML 
+     * should look like this:
+     * <code>
+     *  <mre>
+     *    <d>Base64 encoded data (String)</d>
+     *    <m>Mime Type (String)</m>
+     *  </mre>
+     * </code>
+     * @param xml 
+     * @return An instantiated object or null if the provided XML has errors
+     * @throws ParserConfigurationException - Error in XML parser configuration
+     * @throws IOException -  When the passed string is null (?)
+     * @throws SAXException - When there is some problem with the processed XML
+     */
+    public static MetricResultEntry fromXML(String xml) throws 
+        ParserConfigurationException, SAXException, IOException {
+        
+        MetricResultEntry m = null;
+
+        DocumentBuilderFactory dbfactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = dbfactory.newDocumentBuilder();
+        Document doc = builder.parse(xml);
+
+        String data = doc.getElementsByTagName("d").item(0).getNodeValue();
+        String mime = doc.getElementsByTagName("m").item(0).getNodeValue();
+
+        m = new MetricResultEntry(Base64.decodeBase64(data.getBytes()), mime);
+        
+        return m;
     }
 }
