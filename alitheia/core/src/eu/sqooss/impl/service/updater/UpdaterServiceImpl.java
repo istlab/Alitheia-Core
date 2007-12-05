afabe65dbbca0feb1489f25a1919f795eb0f21d3
Adriaan de Groot <groot@kde.org>
2007-12-05 20:50:59 +0000

    Ignores + make-it-compile.

--- alitheia/core/src/eu/sqooss/impl/service/updater/UpdaterServiceImpl.java
+++ alitheia/core/src/eu/sqooss/impl/service/updater/UpdaterServiceImpl.java
@@ -138,7 +138,7 @@ public class UpdaterServiceImpl extends HttpServlet implements UpdaterService {
             try {
                 SourceUpdater su = new SourceUpdater(path, tdsService,
                         dbService, scheduler, logger);
-                su.doUpdate();
+                su.run();
             } catch (UpdaterException ue) {
                 logger.severe("The Updater failed to update the code for project "
                                 + path);
