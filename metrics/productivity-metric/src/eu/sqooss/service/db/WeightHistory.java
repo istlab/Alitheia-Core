58dc621255456e5f618bce0825cef97913ab4959
Georgios Gousios <gousiosg@aueb.gr>
2007-12-26 19:04:42 +0000

    DAO's and hibernate mappings for the productivity metric results storage

---
+++ metrics/productivity-metric/src/eu/sqooss/service/db/WeightHistory.java
@@ -0,0 +1,70 @@
+/*
+ * This file is part of the Alitheia system, developed by the SQO-OSS
+ * consortium as part of the IST FP6 SQO-OSS project, number 033331.
+ * 
+ * Copyright 2007 by the SQO-OSS consortium members <info@sqo-oss.eu>
+ * Copyright 2007 by Georgios Gousios <gousiosg@gmail.com> 
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ * 
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
+
+package eu.sqooss.service.db;
+
+import java.util.Date;
+
+public class WeightHistory extends DAObject{
+	private long actionID;
+	private long weight;
+	private Date calcDate;
+	
+	public long getActionID() {
+		return actionID;
+	}
+	
+	public void setActionID(long actionID) {
+		this.actionID = actionID;
+	}
+	
+	public long getWeight() {
+		return weight;
+	}
+	
+	public void setWeight(long weight) {
+		this.weight = weight;
+	}
+	
+	public Date getCalcDate() {
+		return calcDate;
+	}
+	
+	public void setCalcDate(Date calcDate) {
+		this.calcDate = calcDate;
+	}
+	
+	
+}
+
+// vi: ai nosi sw=4 ts=4 expandtab
