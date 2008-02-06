bc00d9b37b50a3371f7b2cf9538687a822eddc4e
Vassilios Karakoidas <bkarak@aueb.gr>
2008-02-06 10:42:55 +0000

    * Deleted some semicolons in MailMessage * Introduced Sender Class/DAO for MailingList data according to the D5 database Specification

--- alitheia/core/src/eu/sqooss/service/db/Sender.java
+++ alitheia/core/src/eu/sqooss/service/db/Sender.java
@@ -1,21 +1,22 @@
 /*
  * This file is part of the Alitheia system, developed by the SQO-OSS
  * consortium as part of the IST FP6 SQO-OSS project, number 033331.
- *
+ * 
  * Copyright 2007 by the SQO-OSS consortium members <info@sqo-oss.eu>
+ * Copyright 2007 by Paul J. Adams <paul.adams@siriusit.co.uk> 
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
  * met:
- *
+ * 
  *     * Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
- *
+ * 
  *     * Redistributions in binary form must reproduce the above
  *       copyright notice, this list of conditions and the following
  *       disclaimer in the documentation and/or other materials provided
  *       with the distribution.
- *
+ * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -27,73 +28,34 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
+ * 
  */
- 
 
 package eu.sqooss.service.db;
 
-import java.util.*;
-
 /**
- * DAO Object for the MailMessage database table
- *
+ * DAO Object representation for the Sender
+ * class
+ * 
+ * @author Vassilios Karakoidas (bkarak@aueb.gr)
  */
-public class MailMessage extends DAObject {
-    String sender;
-    String recipients;
-    String listId;
-    String messageId;
-    Date date;
-    String subject;
-
-    public MailMessage() {}
-
-    public String getSender() {
-        return sender;
+public class Sender extends DAObject {
+    String name;
+    String username;
+    
+    public String getName() {
+	return name;
     }
-
-    public void setSender( String value ) {
-        sender = value;
+    
+    public void setName(String name) {
+	this.name = name;
     }
-
-    public String getRecipients() {
-        return recipients;
+    
+    public String getUsername() {
+	return username;
     }
-
-    public void setRecipients( String value ) {
-        recipients = value;
-    }
-
-    public String getListId() {
-        return listId;
-    }
-
-    public void setListId( String value ) {
-        listId = value;
-    }
-
-    public String getMessageId() {
-        return messageId;
-    }
-
-    public void setMessageId( String value ) {
-        messageId = value;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate( Date value ) {
-        date = value;
-    }
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject( String value ) {
-        subject = value;
+    
+    public void setUsername(String username) {
+	this.username = username;
     }
 }
