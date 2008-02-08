996894bad8724397d78187bce2ab3df608fe311d
Panagiotis Louridas <louridas@acm.org>
2008-02-08 12:27:23 +0000

    Accessor methods for BundleContext and DBService

--- alitheia/core/src/eu/sqooss/impl/service/CoreActivator.java
+++ alitheia/core/src/eu/sqooss/impl/service/CoreActivator.java
@@ -4,10 +4,13 @@ import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleActivator;
 
 import eu.sqooss.core.AlitheiaCore;
+import eu.sqooss.service.db.DBService;
 
-public class CoreActivator
-    implements BundleActivator{
+public class CoreActivator implements BundleActivator {
 
+    private static BundleContext bundleContext = null;
+    private static DBService dbService = null;
+    
     public void start(BundleContext bc) throws Exception {
         AlitheiaCore core = new AlitheiaCore(bc);
         bc.registerService(core.getClass().getName(), core, null);
@@ -17,9 +20,20 @@ public class CoreActivator
         
         // Run an instance of the PluginAdmin
         core.initPluginAdmin();
+        
+        bundleContext = bc;
+        dbService = core.getDBService();
     }
 
     public void stop(BundleContext bc) throws Exception {
        
+    }
+    
+    public static BundleContext getBundleContext() {
+        return bundleContext; 
+    }
+    
+    public static DBService getDBService() {
+        return dbService;
     }
 }
