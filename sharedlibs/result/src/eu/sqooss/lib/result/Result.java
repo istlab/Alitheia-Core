c6f39872a720d885c820560881553b269ebe1a97
Georgios Gousios <gousiosg@aueb.gr>
2008-02-08 15:24:03 +0000

    Placeholder for shared core-ui libraries. Also, code pulled in from Evgeni's version of the Result type. Build system to follow

---
+++ sharedlibs/result/src/eu/sqooss/lib/result/Result.java
@@ -0,0 +1,400 @@
+/*
+ * This file is part of the Alitheia system, developed by the SQO-OSS
+ * consortium as part of the IST FP6 SQO-OSS project, number 033331.
+ *
+ * Copyright 2007 by the SQO-OSS consortium members <info@sqo-oss.eu>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package eu.sqooss.lib.result;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+
+
+/**
+ * The WSResult is similar to the MetricResult.
+ * 
+ * <p>
+ * Projects:
+ * The <code>WSResult</code>'s rows consist of fields in the following format:
+     * <p>
+     * <table border=1>
+     *  <tr><td> Field Index </td><td> Field Type </td><td> Field value </td></tr>
+     *  <tr><td> 0 </td><td> type/long    </td><td> id         </td></tr>
+     *  <tr><td> 1 </td><td> text/plain   </td><td> name       </td></tr>
+     *  <tr><td> 2 </td><td> text/plain   </td><td> repository </td></tr>
+     *  <tr><td> 3 </td><td> text/plain   </td><td> bugs       </td></tr>
+     *  <tr><td> 4 </td><td> text/plain   </td><td> mail       </td></tr>
+     *  <tr><td> 5 </td><td> text/plain   </td><td> contact    </td></tr>
+     *  <tr><td> 6 </td><td> text/plain   </td><td> website    </td></tr>
+     *  <tr><td> 7 </td><td> type/integer </td><td> version1   </td></tr>
+     *  <tr><td> 8 </td><td> type/integer </td><td> version2   </td></tr>
+     *  <tr><td>...</td><td> type/integer </td><td> versionN   </td></tr>
+     * <table>
+     * </p><br>
+ * </p>
+ * 
+ * <p>
+ * Metrics:
+ * The <code>Result</code>'s rows consist of fields in the following format:
+ * <p>
+ * <table border=1>
+ *  <tr><td> Field Index </td><td> Field Type </td><td> Field value </td></tr>
+ *  <tr><td> 0 </td><td> type/long    </td><td> id          </td></tr>
+ *  <tr><td> 1 </td><td> text/plain   </td><td> description </td></tr>
+ *  <tr><td> 2 </td><td> text/plain   </td><td> type        </td></tr>
+ * <table>
+ * </p><br>
+ * </p>
+ * 
+ * <p>
+ * Files:
+ * The <code>Result</code>'s rows consist of fields in the following format:
+ * <p>
+ * <table border=1>
+ *  <tr><td> Field Index </td><td> Field Type </td><td> Field value </td></tr>
+ *  <tr><td>  0 </td><td> text/plain   </td><td> name        </td></tr>
+ *  <tr><td>  1 </td><td> text/plain   </td><td> status      </td></tr>
+ *  <tr><td>  2 </td><td> text/plain   </td><td> protection  </td></tr>
+ *  <tr><td>  3 </td><td> type/integer </td><td> links       </td></tr>
+ *  <tr><td>  4 </td><td> type/long    </td><td> user's id   </td></tr>
+ *  <tr><td>  5 </td><td> type/long    </td><td> group's id  </td></tr>
+ *  <tr><td>  6 </td><td> type/long    </td><td> access time </td></tr>
+ *  <tr><td>  7 </td><td> type/long    </td><td> modification time </td></tr>
+ *  <tr><td>  8 </td><td> text/plain   </td><td> file status change </td></tr>
+ *  <tr><td>  9 </td><td> type/integer </td><td> size        </td></tr>
+ *  <tr><td> 10 </td><td> type/integer </td><td> blocks      </td></tr>
+ * </table>
+ * </p><br>
+ * </p>
+ * 
+ * <p>
+ * Users:
+ * The <code>Result</code>'s rows consist of fields in the following format:
+ * <p>
+ * <table border=1>
+ *  <tr><td> Field Index </td><td> Field Type </td><td> Field value </td></tr>
+ *  <tr><td> 0 </td><td> type/long    </td><td> user's id            </td></tr>
+ *  <tr><td> 1 </td><td> text/plain   </td><td> user name            </td></tr>
+ *  <tr><td> 2 </td><td> type/long    </td><td> group1's is          </td></tr>
+ *  <tr><td> 3 </td><td> text/plain   </td><td> groups1' description </td></tr>
+ *  <tr><td> 4 </td><td> type/long    </td><td> group2's is          </td></tr>
+ *  <tr><td> 5 </td><td> text/plain   </td><td> groups2' description </td></tr>
+ *  <tr><td>...</td><td> type/long    </td><td> groupN's is          </td></tr>
+ *  <tr><td>...</td><td> text/plain   </td><td> groupsN' description </td></tr>
+ * </table>
+ * </p><br>
+ * </p>
+ * 
+ */
+public class Result implements Iterable<ArrayList<ResultEntry>>,
+                                          Iterator<ArrayList<ResultEntry>> {
+    
+    private static final String XML_DECLARATION         = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+    private static final String XML_ELEM_NAME_ROOT   = "Result";
+    private static final String XML_ELEM_NAME_ROOT_ROW    = "Row";
+    private static final String XML_ELEM_NAME_ROOT_ROW_FIELD  = "Field";
+    private static final String XML_ELEM_NAME_ROOT_ROW_FIELD_MIME  = "MimeType";
+    private static final String XML_ELEM_NAME_ROOT_ROW_FIELD_VALUE = "Value";
+    
+    private ArrayList<ArrayList<ResultEntry>> ResultTable;
+    private int currentRow;
+
+    private Object lockObject = new Object();
+
+    private static SAXParser saxParser;
+    private static SAXDefaultHandler saxHandler = new SAXDefaultHandler(XML_ELEM_NAME_ROOT_ROW,
+            XML_ELEM_NAME_ROOT_ROW_FIELD, XML_ELEM_NAME_ROOT_ROW_FIELD_MIME,
+            XML_ELEM_NAME_ROOT_ROW_FIELD_VALUE);
+    
+    public Result() {
+        ResultTable = new ArrayList<ArrayList<ResultEntry>>();
+        currentRow = -1;
+    }
+
+    public Result(String plainText) {
+        this();
+        if (plainText != null) {
+            ArrayList<ResultEntry> row = new ArrayList<ResultEntry>(1);
+            row.add(new ResultEntry(plainText, ResultEntry.MIME_TYPE_TEXT_PLAIN));
+            addResultRow(row);
+        }
+    }
+    
+    public Result(long longValue) {
+        this();
+        ArrayList<ResultEntry> row = new ArrayList<ResultEntry>(1);
+        row.add(new ResultEntry(longValue, ResultEntry.MIME_TYPE_TYPE_LONG));
+        addResultRow(row);
+    }
+    
+    /* Iterator's methods */
+    /**
+     * @see java.util.Iterator#hasNext()
+     */
+    public boolean hasNext() {
+        synchronized (lockObject) {
+            if (currentRow < (ResultTable.size()-1)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    /**
+     * @see java.util.Iterator#next()
+     */
+    public ArrayList<ResultEntry> next() {
+        synchronized (lockObject) {
+            if (hasNext()) {
+                currentRow++;
+                return ResultTable.get(currentRow);
+            } else {
+                throw new NoSuchElementException("There are no more elements!");
+            }
+        }
+    }
+
+    /**
+     * @see java.util.Iterator#remove()
+     */
+    public void remove() {
+        throw new UnsupportedOperationException("The remove operation is unsupported!");
+    } 
+    /* Iterator's methods */
+    
+    /* Iterable's method */
+    /**
+     * @see java.lang.Iterable#iterator()
+     */
+    public Iterator<ArrayList<ResultEntry>> iterator() {
+        return this;
+    }
+    /* Iterable's method */
+    
+    /**
+     * This method is useful to the iteration process.
+     * It moves the cursor before first row in the <code>Result</code>.
+     */
+    public void beforeFirst() {
+        synchronized (lockObject) {
+            currentRow = -1;
+        }
+    }
+    
+    /**
+     * This method moves the cursor to the first row in the <code>Result</code>.
+     * If the <code>Result</code> is empty then the method is similar to the {@link #beforeFirst()}
+     */
+    public void first() {
+        synchronized (lockObject) {
+            if (ResultTable.isEmpty()) {
+                beforeFirst();
+            } else {
+                currentRow = 0;
+            }
+        }
+    }
+
+    /**
+     * This method moves the cursor to the last row in the <code>Result</code>.
+     * If the <code>Result</code> is empty the the method is similar to the {@link #beforeFirst()}
+     */
+    public void last() {
+        synchronized (lockObject) {
+            currentRow = ResultTable.size()-1;
+        }
+    }
+    
+    /**
+     * Returns the current row.
+     * @return The row at the current position of the cursor.
+     * @throws IllegalStateException if the Result is empty.
+     */
+    public ArrayList<ResultEntry> get() {
+        synchronized (lockObject) {
+            if (currentRow == -1) {
+                throw new IllegalStateException("Can't get the row with index -1!");
+            } else {
+                return ResultTable.get(currentRow);
+            }
+        }
+    }
+
+    /**
+     * Returns the specified row
+     * @param i index of row to return
+     * @return The specified row
+     * @throws IndexOutOfBoundsException if the row index is out of range
+     */
+    public ArrayList<ResultEntry> getRow(int i) {
+        return ResultTable.get(i);
+    }
+
+    /**
+     * Returns the specified result table cell
+     * @param x The result table row
+     * @param y The result table column
+     * @return The requested field
+     * @throws IndexOutOfBoundsException if <tt>x</tt> or <tt>y</tt> is out 
+     * of range 
+     */
+    public ResultEntry getFieldAt(int x, int y) {
+        ArrayList<ResultEntry> line = getRow(x);
+        return line.get(y);
+    }
+
+    /**
+     * Appends a result row to the end of results table
+     * @param result The result row to add
+     */
+    public void addResultRow(ArrayList<ResultEntry> result) {
+        synchronized (lockObject) {
+            this.ResultTable.add(result);
+        }
+    }
+
+    /**
+     * Return the number of rows in the the MetricResult
+     * @return The number of rows
+     */
+    public int getRowCount() {
+        return ResultTable.size();
+    }
+
+    /**
+     * This method returns the XML representation of the <code>Result</code>.
+     * 
+     * @return - the string represents the Result in XML format
+     */
+    public String toXML() {
+        String rootStartTag     = "<" + XML_ELEM_NAME_ROOT + ">\n";
+        String rootEndTag       = "</" + XML_ELEM_NAME_ROOT + ">\n";;
+        String rowStartTag      = "\t<" + XML_ELEM_NAME_ROOT_ROW + ">\n";
+        String rowEndTag        = "\t</" + XML_ELEM_NAME_ROOT_ROW + ">\n";
+        String fieldStartTag    = "\t\t<" + XML_ELEM_NAME_ROOT_ROW_FIELD + ">\n";
+        String fieldEndTag      = "\t\t</" + XML_ELEM_NAME_ROOT_ROW_FIELD + ">\n";
+        String mimeTypeStartTag = "\t\t\t<" + XML_ELEM_NAME_ROOT_ROW_FIELD_MIME + ">\n";
+        String mimeTypeEndTag   = "\t\t\t</" + XML_ELEM_NAME_ROOT_ROW_FIELD_MIME + ">\n";
+        String valueStartTag    = "\t\t\t<" + XML_ELEM_NAME_ROOT_ROW_FIELD_VALUE + ">\n";
+        String valueEndTag      = "\t\t\t</" + XML_ELEM_NAME_ROOT_ROW_FIELD_VALUE + ">\n";
+        String ResultEntryIndentation = "\t\t\t\t";
+        StringBuffer result   = new StringBuffer();
+        result.append(XML_DECLARATION);
+        result.append(rootStartTag);
+        for (ArrayList<ResultEntry> currentRow : ResultTable) {
+            result.append(rowStartTag);
+            for (ResultEntry currentField : currentRow) {
+                result.append(fieldStartTag);
+                
+                result.append(mimeTypeStartTag);
+                result.append(ResultEntryIndentation);
+                result.append(currentField.getMimeType());
+                result.append('\n');
+                result.append(mimeTypeEndTag);
+                
+                result.append(valueStartTag);
+                result.append(ResultEntryIndentation);
+                result.append(currentField.toString());
+                result.append('\n');
+                result.append(valueEndTag);
+                
+                result.append(fieldEndTag);
+            }
+            result.append(rowEndTag);
+        }
+        result.append(rootEndTag);
+        return result.toString();
+    }
+
+    /**
+     * This method creates a new object from a XML description.
+     * 
+     * @param xml
+     * @return
+     * @throws WSException
+     */
+    public static Result fromXML(String xml) throws Exception { 
+        try {
+            if (saxParser == null) {
+                SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
+                SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+                //TODO: set the xml schema file
+                Schema ResultSchema = schemaFactory.newSchema(new File("./Result.xsd"));
+                saxParserFactory.setValidating(false);
+                saxParserFactory.setSchema(ResultSchema);
+                saxParser = saxParserFactory.newSAXParser();
+            }
+            String xmlEncoding = getEncodingFromXMLDeclaration(xml);
+            byte[] xmlInBytes;
+            try {
+                xmlInBytes = xml.getBytes(xmlEncoding);
+            } catch (UnsupportedEncodingException uee) {
+                xmlInBytes = xml.getBytes("UTF-8");
+            }
+            InputStream xmlInputStream = new ByteArrayInputStream(xmlInBytes);            
+            saxParser.parse(xmlInputStream, saxHandler);
+            return saxHandler.getParsedResult();
+        } catch(Exception e) {
+            //TODO: Add resultlib specific exception
+            throw new Exception(e);
+        }
+    }
+    
+    /**
+     * This method returns the encoding from the XML's declaration.
+     * If the encoding is missing then returns UTF-8.
+     * @param xml
+     * @return
+     */
+    private static String getEncodingFromXMLDeclaration(String xml) {
+        String encoding = "UTF-8";
+        StringBuffer xmlDeclaration = new StringBuffer(xml.substring(0, xml.indexOf('>') + 1));
+        int encodingIndex = xmlDeclaration.indexOf("encoding");
+        if (encodingIndex != -1) {
+            xmlDeclaration.delete(0, encodingIndex);
+            xmlDeclaration.delete(0, xmlDeclaration.indexOf("\"") + 1);
+            encoding = xmlDeclaration.substring(0, xmlDeclaration.indexOf("\"")).trim();
+        }
+        return encoding;
+    }
+}
+
+//vi: ai nosi sw=4 ts=4 expandtab
---
+++ sharedlibs/result/src/eu/sqooss/lib/result/SAXDefaultHandler.java
@@ -0,0 +1,147 @@
+/*
+ * This file is part of the Alitheia system, developed by the SQO-OSS
+ * consortium as part of the IST FP6 SQO-OSS project, number 033331.
+ *
+ * Copyright 2007 by the SQO-OSS consortium members <info@sqo-oss.eu>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package eu.sqooss.lib.result;
+
+import java.util.ArrayList;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * This class has a package visibility. 
+ */
+class SAXDefaultHandler extends DefaultHandler {
+    
+    private String rowElemName;
+    private String fieldElemName;
+    private String mimeTypeElemName;
+    private String valueElemName;
+    
+    private String currentElemName;
+    private StringBuffer currentMimeTypeElemVal;
+    private StringBuffer currentValueElemVal;
+    
+    private Result tableResult;
+    private ArrayList<ResultEntry> currentRow;
+
+    public SAXDefaultHandler(String rowElemName, String fieldElemName,
+            String mimeTypeElemName, String valueElemName) {
+        this.rowElemName = rowElemName;
+        this.fieldElemName = fieldElemName;
+        this.mimeTypeElemName = mimeTypeElemName;
+        this.valueElemName = valueElemName;
+        
+        currentMimeTypeElemVal = new StringBuffer();
+        currentValueElemVal = new StringBuffer();
+    }
+    
+    /**
+     * @see org.xml.sax.helpers.DefaultHandler#startDocument()
+     */
+    @Override
+    public void startDocument() throws SAXException {
+        super.startDocument();
+        tableResult = new Result();
+    }
+
+    /**
+     * @see org.xml.sax.helpers.DefaultHandler#startElement(java.lang.String, java.lang.String, java.lang.String, org.xml.sax.Attributes)
+     */
+    @Override
+    public void startElement(String uri, String localName, String name,
+            Attributes attributes) throws SAXException {
+        super.startElement(uri, localName, name, attributes);
+        
+        if (rowElemName.equals(name)) {
+            currentRow = new ArrayList<ResultEntry>();
+        }
+        currentElemName = name;
+    }
+    
+    /**
+     * @see org.xml.sax.helpers.DefaultHandler#characters(char[], int, int)
+     */
+    @Override
+    public void characters(char[] ch, int start, int length)
+            throws SAXException {
+        super.characters(ch, start, length);
+        
+        if (mimeTypeElemName.equals(currentElemName)) {
+            currentMimeTypeElemVal.append(new String(ch, start, length).trim());
+        } else if (valueElemName.equals(currentElemName)) {
+            currentValueElemVal.append(new String(ch, start, length).trim());
+        }
+    }
+    
+    /**
+     * @see org.xml.sax.helpers.DefaultHandler#endElement(java.lang.String, java.lang.String, java.lang.String)
+     */
+    @Override
+    public void endElement(String uri, String localName, String name)
+            throws SAXException {
+        super.endElement(uri, localName, name);
+        
+        if (rowElemName.equals(name)) {
+            tableResult.addResultRow(currentRow);
+            currentRow = null;
+        } else if (fieldElemName.equals(name)) {
+            String mimeType = currentMimeTypeElemVal.toString();
+            currentRow.add(ResultEntry.fromString(currentValueElemVal.toString(), mimeType));
+            currentValueElemVal.setLength(0);
+            currentMimeTypeElemVal.setLength(0);
+        }
+    }
+
+    /**
+     * This method returns the last parsed WSResult or null if there isn't.
+     * 
+     * @return
+     */
+    public Result getParsedResult() {
+        return tableResult;
+    }
+
+    /**
+     * @see org.xml.sax.helpers.DefaultHandler#error(org.xml.sax.SAXParseException)
+     */
+    @Override
+    public void error(SAXParseException e) throws SAXException {
+        super.error(e);
+        throw e;
+    }
+
+}
+
+//vi: ai nosi sw=4 ts=4 expandtab
