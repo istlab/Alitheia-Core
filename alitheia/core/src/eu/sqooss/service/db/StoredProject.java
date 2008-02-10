edece248aaf1049e43fd4ba02f6797e42801a34d
Adriaan de Groot <groot@kde.org>
2008-02-10 10:42:07 +0000

    r5818@admins-computer:  adridg | 2008-02-10 11:41:56 +0100  For the first time I agree with D5's statement that Java programmers can't write SQL. This is still very broken and susceptible to 'Little Bobby Tables' injections. Fixed column name, fixed use of string.

--- alitheia/core/src/eu/sqooss/service/db/StoredProject.java
+++ alitheia/core/src/eu/sqooss/service/db/StoredProject.java
@@ -1,23 +1,23 @@
 /*
  * This file is part of the Alitheia system, developed by the SQO-OSS
  * consortium as part of the IST FP6 SQO-OSS project, number 033331.
- * 
+ *
  * Copyright 2007 by the SQO-OSS consortium members <info@sqo-oss.eu>
  * Copyright 2007 by Adriaan de Groot <groot@kde.org>
  * Copyright 2007 by Paul J. Adams <paul.adams@siriusit.co.uk>
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
  * met:
- * 
+ *
  *     * Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
- * 
+ *
  *     * Redistributions in binary form must reproduce the above
  *       copyright notice, this list of conditions and the following
  *       disclaimer in the documentation and/or other materials provided
  *       with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -29,7 +29,7 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  */
 
 
@@ -52,7 +52,7 @@ import eu.sqooss.service.logging.Logger;
  */
 public class StoredProject extends DAObject {
     /**
-     * Public, human-readable name of the project (e.g. Evolution, 
+     * Public, human-readable name of the project (e.g. Evolution,
      * GNOME, Catalina, Sciplot). Used for display purposes.
      */
     private String name;
@@ -133,12 +133,12 @@ public class StoredProject extends DAObject {
     public void setMail(String url) {
         this.mailUrl = url;
     }
-    
+
     public static StoredProject getProjectByName(String name, Logger logger) {
         StoredProject project = null;
         DBService dbs = CoreActivator.getDBService();
-        
-        List prList = dbs.doHQL("from StoredProject where Name = " + name + "");
+
+        List prList = dbs.doHQL("from StoredProject where PROJECT_NAME = '" + name + "'");
         if ((prList == null) || (prList.size() != 1)) {
             logger.error("The requested project was not found");
             return null;
@@ -147,7 +147,7 @@ public class StoredProject extends DAObject {
         project = (StoredProject) prList.get(0);
         return project;
     }
-    
+
     public static ProjectVersion getLastProjectVersion(StoredProject project, Logger logger) {
         ProjectVersion lastVersion = null;
         DBService dbs = CoreActivator.getDBService();
@@ -169,7 +169,7 @@ public class StoredProject extends DAObject {
         }
         return lastVersion;
     }
-    
+
     public List<ProjectVersion> getProjectVersions() {
         //TODO: implement
         return null;
