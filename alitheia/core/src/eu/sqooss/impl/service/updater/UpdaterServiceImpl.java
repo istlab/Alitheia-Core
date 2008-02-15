d1b2e2a5ac8b24af82c19e05296adb8e1e040b01
Adriaan de Groot <groot@kde.org>
2008-02-15 11:52:33 +0000

    r6183@admins-computer:  adridg | 2008-02-15 12:49:57 +0100  Avoid null pointer crashes and format more nicely.  r6184@admins-computer:  adridg | 2008-02-15 12:52:12 +0100  Ignores

--- alitheia/core/src/eu/sqooss/impl/service/updater/UpdaterServiceImpl.java
+++ alitheia/core/src/eu/sqooss/impl/service/updater/UpdaterServiceImpl.java
@@ -240,7 +240,7 @@ public class UpdaterServiceImpl extends HttpServlet implements UpdaterService {
         }
 
         Set<UpdateTarget> s = currentJobs.get(project.getName());
-        logger.info("Update set is:" + s.toString());
+        logger.info("Update set is:" + explain(s));
        
         // Check all the types we need and make claims
         synchronized(currentJobs) {
