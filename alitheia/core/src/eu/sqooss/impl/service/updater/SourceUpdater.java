888da06e2ae1f6e6401ea6247c25a3fc0be33084
Georgios Gousios <gousiosg@aueb.gr>
2008-04-14 16:17:51 +0000

    When an svn path does not include the path delimeter assume it is refering to the root path

--- alitheia/core/src/eu/sqooss/impl/service/updater/SourceUpdater.java
+++ alitheia/core/src/eu/sqooss/impl/service/updater/SourceUpdater.java
@@ -192,6 +192,8 @@ class SourceUpdater extends Job {
                     
                     ProjectFile pf = curVersion.addProjectFile();
                     String path = chPath.substring(0, chPath.lastIndexOf('/'));
+                    if (path == "")
+                        path = "/"; //SVN entry does not have a path
                     String fname = chPath.substring(chPath.lastIndexOf('/') + 1);
 
                     Directory dir = null;
