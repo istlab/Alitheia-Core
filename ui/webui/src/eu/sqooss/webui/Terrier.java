e3259b92c5516d9953af6dd796d8889c8712173b
Yotov Boryan <b.yotov@prosyst.com>
2008-04-15 15:35:43 +0000

    Init the configuration first, then connect.

--- ui/webui/src/eu/sqooss/webui/Terrier.java
+++ ui/webui/src/eu/sqooss/webui/Terrier.java
@@ -84,8 +84,8 @@ public class Terrier {
      * Simple constructor. Instantiates a new <code>Terrier</code> object.
      */
     public Terrier () {
-        connect();
         initConfig();
+        connect();
     }
 
     private void initConfig() {
