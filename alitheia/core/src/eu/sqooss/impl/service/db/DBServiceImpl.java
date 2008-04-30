ab5f5b907bfecfba374911011db798ef78a79c27
Romain Pokrzywka <romain@kdab.net>
2008-04-30 21:38:00 +0000

    log user errors as warnings.

--- alitheia/core/src/eu/sqooss/impl/service/db/DBServiceImpl.java
+++ alitheia/core/src/eu/sqooss/impl/service/db/DBServiceImpl.java
@@ -443,10 +443,10 @@ public class DBServiceImpl implements DBService {
             // We use "foo" as the name of the object
             return (List<T>) doHQL( s, "from " + daoClass.getName() + " as foo " + whereClause, parameterMap );
         } catch (QueryException e) {
-            logger.error("Invalid property name: " + e.getMessage());
+            logger.warning("Invalid property name: " + e.getMessage());
             return Collections.emptyList();
         } catch (ClassCastException e) {
-            logger.error("Invalid property type: " + e.getMessage());
+            logger.warning("Invalid property type: " + e.getMessage());
             return Collections.emptyList();
         }
     }
@@ -544,7 +544,7 @@ public class DBServiceImpl implements DBService {
             logger.error("Hibernate error: " + e.getMessage());
             throw e;
         } catch (ClassCastException e) {
-            logger.error("Invalid SQL query parameter type: " + e.getMessage());
+            logger.warning("Invalid SQL query parameter type: " + e.getMessage());
             throw e;
         }
     }
@@ -634,13 +634,13 @@ public class DBServiceImpl implements DBService {
             logSQLException(e.getSQLException());
             throw e;
         } catch (QueryException e) {
-            logger.error("Error while executing HQL query: " +  e.getMessage());
+            logger.warning("Error while executing HQL query: " +  e.getMessage());
             throw e;
         } catch (HibernateException e) {
             logger.error("Hibernate error: " + e.getMessage());
             throw e;
         } catch (ClassCastException e) {
-            logger.error("Invalid HQL query parameter type: " + e.getMessage());
+            logger.warning("Invalid HQL query parameter type: " + e.getMessage());
             throw e;
         }
         
