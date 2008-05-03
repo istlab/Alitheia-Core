83c76ffcecc65fd204583b36d60739eafc43efbf
Yotov Boryan <b.yotov@prosyst.com>
2008-05-03 10:35:57 +0000

    Some API doc

--- alitheia/core/src/eu/sqooss/service/security/SecurityManager.java
+++ alitheia/core/src/eu/sqooss/service/security/SecurityManager.java
@@ -35,79 +35,109 @@ package eu.sqooss.service.security;
 import java.util.Dictionary;
 
 /**
- * The <code>SecurityManager</code> class is used for validating the access to the SQO-OSS resources and
- * their management.
+ * The <code>SecurityManager</code> class is used for validating the access
+ * to the SQO-OSS resources, and provides methods for creation and management
+ * of access privileges.
  */
 public interface SecurityManager {
 
     /**
-     * Validate the access to the SQO-OSS resource based on the full URL (with privileges), user name and password.
-     * @param fullURL the full URL contains the privileges
-     * @param userName
-     * @param password
-     * @return <code>true</code> if the access is allowed, <code>false</code> otherwise
+     * Validates the access to a SQO-OSS resource located by the given
+     * resource's URL. The URL must include the access privileges as well.
+     * The validation process is then performed against the specified user
+     * name and password.
+     * 
+     * @param fullURL - the URL location (plus access privileges) of the
+     *   requested resource
+     * @param privileges - a list of access privileges for that resource 
+     * @param userName - user account's name
+     * @param password - user account's password
+     * @return <code>true</code> if the set of access privileges can be
+     *   granted for that resource with the specified user account,
+     *   or <code>false</code> otherwise.
      */
     public boolean checkPermission(String fullURL, String userName, String password);
 
     /**
-     * Validates the access to the SQO-OSS resource based on the resourceURL, privileges, user name and password.
-     * @param resourceURL
-     * @param privileges
-     * @param userName
-     * @param password
-     * @return <code>true</code> if the access is allowed, <code>false</code> otherwise
+     * Validates the access to a SQO-OSS resource located by the given
+     * resource's URL. The URL must not include access privileges, since they
+     * are provided in additional parameter. The validation process is then
+     * performed against the specified user name and password.
+     * 
+     * @param resourceURL - the URL location of the requested resource
+     * @param privileges - a list of access privileges for that resource 
+     * @param userName - user account's name
+     * @param password - user account's password
+     * @return <code>true</code> if the set of access privileges can be
+     *   granted for that resource with the specified user account,
+     *   or <code>false</code> otherwise.
      */
     public boolean checkPermission(String resourceUrl, Dictionary<String, String> privileges, String userName, String password);
 
     /**
-     * This method returns the group manager.
-     * @return the group manager
+     * This method returns the locally stored <code>GroupManager</code>
+     * instance.
+     * 
+     * @return The group manager instance.
      * @see eu.sqooss.service.security.GroupManager
      */
     public GroupManager getGroupManager();
-    
+
     /**
-     * This method returns the privilege manager.
-     * @return the privilege manager
+     * This method returns the locally stored <code>PrivilegeManager</code>
+     * instance.
+     * 
+     * @return The privilege manager instance.
      * @see eu.sqooss.service.security.PrivilegeManager
      */
     public PrivilegeManager getPrivilegeManager();
-    
+
     /**
-     * This method returns the user manager.
-     * @return the user manager
+     * This method returns the locally stored <code>UserManager</code>
+     * instance.
+     * 
+     * @return The user manager instance.
      * @see eu.sqooss.service.security.UserManager
      */
     public UserManager getUserManager();
-    
+
     /**
-     * This method returns the service url manager.
-     * @return the service url manager
+     * This method returns the locally stored <code>ServiceUrlManager</code>
+     * instance.
+     * 
+     * @return The service URL manager instance.
      * @see eu.sqooss.service.security.ServiceUrlManager
      */
     public ServiceUrlManager getServiceUrlManager();
-    
+
     /**
-     * This method creates a new security configuration.
-     * If some of the arguments doesn't exist then they are created.
-     * @param groupDescription
-     * @param privilege
-     * @param privilegeValue
-     * @param serviceUrl
-     * @return <code>true</code> if the configuration is created,
-     * <code>false</code> otherwise
+     * This method creates a new access model for the SQO-OSS resource located
+     * by the given URL. Access to that resource are granted or denied for
+     * the specified group depending on the given access privilege.
+     * <br/>
+     * <i>When some of the arguments (like group or privilege) doesn't have
+     *   an associated record in the underlying database, then the
+     *   corresponding record(s) is(are) created automatically.</i>
+     * 
+     * @param groupDescription -- group name
+     * @param privilege -- privilege name
+     * @param privilegeValue -- privilege value
+     * @param serviceUrl -- the URL location of the affected resource
+     * @return <code>true</code> if the access model is successfully created,
+     *   or <code>false</code> otherwise
      */
     public boolean createSecurityConfiguration(String groupDescription,
             String privilege, String privilegeValue, String serviceUrl);
-    
+
     /**
-     * This method deletes the security configuration.
-     * @param groupDescription
-     * @param privilege
-     * @param privilegeValue
-     * @param serviceUrl
-     * @return <code>true</code> if the configuration is deleted,
-     * <code>false</code> otherwise
+     * This method deletes an existing access model.
+     * 
+     * @param groupDescription -- group name
+     * @param privilege -- privilege name
+     * @param privilegeValue -- privilege value
+     * @param serviceUrl -- the URL location of the affected resource
+     * @return <code>true</code> if the specified access model is successfully
+     *   deleted, or <code>false</code> otherwise
      */
     public boolean deleteSecurityConfiguration(String groupDescription,
             String privilege, String privilegeValue, String serviceUrl);
