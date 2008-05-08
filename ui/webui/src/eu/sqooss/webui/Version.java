cf922f2cd620fa764f468604e0207ed38f6a3c76
Sebastian Kugler <sebas@kde.org>
2008-05-08 13:38:19 +0000

    Version is the webui wrapper for WSStoredProjectVersion. It's incomplete yet, some changes in the SCL are needed.

---
+++ ui/webui/src/eu/sqooss/webui/Version.java
@@ -0,0 +1,76 @@
+/*
+ * This file is part of the Alitheia system, developed by the SQO-OSS
+ * consortium as part of the IST FP6 SQO-OSS project, number 033331.
+ *
+ * Copyright 2008 by the SQO-OSS consortium members <info@sqo-oss.eu>
+ * Copyright 2008 by Sebastian Kuegler <sebas@kde.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package eu.sqooss.webui;
+
+import eu.sqooss.ws.client.datatypes.WSProjectVersion;
+
+import eu.sqooss.webui.Project;
+
+
+class Version {
+
+    private static final String COMMENT = "<!-- Version -->\n";
+    private Project project;
+    private Long number;
+    private Long id;
+
+    public Version (WSProjectVersion wsVersion) {
+        //project = new Project(wsVersion.getProject()); // FIXME: pending "getProject returns ID"
+        id = wsVersion.getId();
+        number = new Long(99999999); // TODO;
+    }
+
+    public Long getId () {
+        return id;
+    }
+
+    public File[] getFiles () {
+        // TODO
+        return null;
+    }
+
+    public String getHtml() {
+        StringBuilder html = new StringBuilder(COMMENT);
+        html.append("<b>Version:</b> " + id);
+        return html.toString();
+    }
+
+    public String shortName () {
+        return "v" + number;
+    }
+
+    public String longName () {
+        return getHtml(); // Yeah, we'r lazy.
+    }
+}
