f34def99998162063d256250bd57cb89f18e1b58
Kevin Ottens <ervin@kde.org>
2008-05-19 14:41:51 +0000

    Forgot a file... sorry.

--- ui/scl/sclib/spec/spec/Scl.java
+++ ui/scl/sclib/spec/spec/Scl.java
@@ -3,8 +3,14 @@ package spec;
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
 
+import eu.sqooss.scl.WSException;
+import eu.sqooss.scl.WSSession;
+
 @RunWith(ConcordionRunner.class)
 public class Scl
 {
-
+	public void checkConnection() throws WSException
+	{
+		new WSSession("alitheia", "alitheia", "http://localhost:8088/sqooss/services/ws/");
+	}
 }
