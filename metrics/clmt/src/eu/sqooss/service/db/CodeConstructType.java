34431824da5dde23090f37ae169a36a0559c2726
Georgios Gousios <gousiosg@aueb.gr>
2008-05-21 09:35:35 +0000

    DAOs for CLMT storage

---
+++ metrics/clmt/src/eu/sqooss/service/db/CodeConstructType.java
@@ -0,0 +1,92 @@
+/*
+ * This file is part of the Alitheia system, developed by the SQO-OSS
+ * consortium as part of the IST FP6 SQO-OSS project, number 033331.
+ *
+ * Copyright 2007-2008 by the SQO-OSS consortium members <info@sqo-oss.eu>
+ * Copyright 2007-2008 by Georgios Gousios <gousiosg@gmail.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package eu.sqooss.service.db;
+
+import java.util.HashMap;
+import java.util.List;
+
+import eu.sqooss.impl.service.CoreActivator;
+
+public class CodeConstructType extends DAObject {
+    
+    public enum ConstructType {
+        
+        CLASS, STATEMENT, METHOD, ATTRIBUTE;
+            
+        public static ConstructType fromString(String s) {
+            if ("CLASS".equals(s))
+                return ConstructType.CLASS;
+            else if ("STATEMENT".equals(s))
+                return ConstructType.STATEMENT;
+            else if ("METHOD".equals(s))
+                return ConstructType.METHOD;
+            else if ("ATTRIBUTE".equals(s))
+                return ConstructType.ATTRIBUTE;
+            else
+                return null;
+        }
+    }
+    
+    private String type;
+   
+    public CodeConstructType(ConstructType t) {
+        type = t.toString();
+    }
+
+    public ConstructType getConstructType() {
+        return ConstructType.fromString(type);
+    }
+    
+    public String getType(){
+        return type;
+    }
+    
+    public void setType(String s) {
+        this.type = ConstructType.fromString(s).toString();
+    }
+    
+    public static CodeConstructType getMetricType(ConstructType t) {
+        DBService db = CoreActivator.getDBService();
+        HashMap<String, Object> s = new HashMap<String, Object>();
+        s.put("type", t.toString());
+        List<CodeConstructType> result = 
+            db.findObjectsByProperties(CodeConstructType.class, s);
+        if (result.isEmpty()) {
+            return null;
+        }
+        else {
+            return (CodeConstructType) result.get(0);
+        }
+    }
+}
