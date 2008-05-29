3de6c14bab1b15ece0838d42984c9cec0d6ad837
Christoph Schleifenbaum <christoph@kdab.net>
2008-05-29 18:46:57 +0000

    Python fun

--- corba/python/test.py
+++ corba/python/test.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python
 
-from logger import *
+from alitheia import Logger
+from alitheia import Core
+from alitheia import Job
+from alitheia import Scheduler
 
 l = Logger( "sqooss" )
 l.error( "Test" )
+
+j = Job()
+s = Scheduler()
+s.enqueueJob( j )
+
+Core.shutdown()
