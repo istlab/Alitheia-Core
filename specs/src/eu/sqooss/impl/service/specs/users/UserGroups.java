b906c3c5ab09a46915a368ea2a88c50f4b7b4682
Kevin Ottens <ervin@kde.org>
2008-06-02 14:14:32 +0000

    Removing useless code. That'll teach me to copy the SpecExample and forget to clean it. :-)

--- specs/src/eu/sqooss/impl/service/specs/users/UserGroups.java
+++ specs/src/eu/sqooss/impl/service/specs/users/UserGroups.java
@@ -1,42 +1,9 @@
 package eu.sqooss.impl.service.specs.users;
 
-import java.util.Collection;
-import java.util.ArrayList;
-
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
 
 @RunWith(ConcordionRunner.class)
 public class UserGroups
 {
-    public String status()
-    {
-        return "connected";
-    }
-
-    public Collection<Person> getPeople()
-    {
-        return new ArrayList<Person>() {
-        	public static final long serialVersionUID = -1;
-        	{
-            add(new Person("Peter", "Gibbons"));
-            add(new Person("Samir", "Nagheenanajar"));
-            add(new Person("Michael", "Bolton"));
-            add(new Person("Milton", "Waddams"));
-        	}
-        };
-    }
-
-    class Person
-    {
-
-        public Person(String firstName, String lastName)
-        {
-            this.firstName = firstName;
-            this.lastName = lastName;
-        }
-
-        public String firstName;
-        public String lastName;
-    }
 }
