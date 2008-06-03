2fadb3cafbcf877df8c468581dc49a495a3d007e
Christoph Schleifenbaum <christoph@kdab.net>
2008-06-03 17:14:00 +0000

    Little Metric skeleton running in Python

--- corba/python/test.py
+++ corba/python/test.py
@@ -8,10 +8,26 @@ from alitheia import ProjectFileMetric
 
 class TestJob(Job):
     def run(self):
-        print "run!"
+        print 'run!'
 
-l = Logger( "sqooss" )
-l.error( "Test" )
+class Metric(ProjectFileMetric):
+    def run(self,file):
+        print file
+
+    def name(self):
+        return 'TestMetric'
+
+    def install(self):
+        return True
+
+    def remove(self):
+        return True
+
+    def version(self):
+        return '1.0.0.0'
+
+l = Logger('sqooss')
+l.error('Test')
 
 j = TestJob()
 s = Scheduler()
@@ -19,9 +35,9 @@ s.enqueueJob( j )
 
 c = Core()
 
-m = ProjectFileMetric()
+m = Metric()
 c.registerMetric(m)
 
 j.waitForFinished()
 
-Core.shutdown()
+#Core.shutdown()
