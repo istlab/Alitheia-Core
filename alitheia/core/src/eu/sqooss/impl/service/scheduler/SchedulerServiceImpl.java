246382783ca6de2dbb61f9b01c81b81e44ad6baf
Christoph Schleifenbaum <christoph@kdab.net>
2008-06-11 12:40:00 +0000

    Oooops... _that_ was just supposed to be just temporarily

--- alitheia/core/src/eu/sqooss/impl/service/scheduler/SchedulerServiceImpl.java
+++ alitheia/core/src/eu/sqooss/impl/service/scheduler/SchedulerServiceImpl.java
@@ -69,7 +69,7 @@ public class SchedulerServiceImpl implements Scheduler {
         logger = l;
         logger.info("Got scheduling!");
 
-        startExecute(1);//3 * Runtime.getRuntime().availableProcessors());
+        startExecute(3 * Runtime.getRuntime().availableProcessors());
     }
 
     synchronized public void enqueue(Job job) throws SchedulerException {
