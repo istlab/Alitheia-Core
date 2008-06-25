0184354dee951e52515aaca662a60c3971e04991
Kevin Ottens <ervin@kde.org>
2008-06-25 13:53:20 +0000

    Add the missing files... Now really begins to explore the project source history. Might fail for you since it assumes some test data to be available.

---
+++ specs/src/eu/sqooss/impl/service/specs/projects/scm/ScmExplore.java
@@ -0,0 +1,26 @@
+package eu.sqooss.impl.service.specs.projects.scm;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+import eu.sqooss.impl.service.dsl.SpProject;
+import eu.sqooss.service.tds.InvalidRepositoryException;
+
+@RunWith(ConcordionRunner.class)
+public class ScmExplore
+{
+    public void addProject(String projectName, String scmPath) throws IOException
+    {
+        SpProject project = new SpProject(projectName);
+        project.repository = "file://"+new File(scmPath).getCanonicalPath();
+        project.create();
+    }
+    
+    public boolean projectHasHistory(String projectName) throws InvalidRepositoryException
+    {
+        return !new SpProject(projectName).revisions().isEmpty();
+    }
+}
