5b2d9e0a93f926226161f96c2dce9dc1c2389c3f
Adriaan de Groot <groot@kde.org>
2008-09-02 13:03:52 +0000

    Specs for the file type determination code (yay, finally a non SFFF spec) which show that one kind of file detection is weird (for locale-files) and that you can't use split to get a 0-length array.

--- specs/src/eu/sqooss/impl/service/specs/util/FileType.java
+++ specs/src/eu/sqooss/impl/service/specs/util/FileType.java
@@ -17,15 +17,63 @@ public class FileType
         return "connected";
     }
 
+    public int splitTest() {
+    	return ":".split(":",-1).length;
+    }
+    
+    public int splitTestEmpty() {
+    	return "".split(":",-1).length;
+    }
+    
     public Collection<FileExtension> getFiles()
     {
         return new ArrayList<FileExtension>() {
        	    public static final long serialVersionUID = -1;
        	    {
+       	    	// Just some regular filenames
                 add(new FileExtension("foo.c"));
                 add(new FileExtension("foo.h"));
                 add(new FileExtension("foo.moose"));
+                add(new FileExtension("foo.moose.c"));
+                add(new FileExtension("foo.c.moose"));
                 add(new FileExtension("foo.tar.gz"));
+                // Unusual numbers of dots
+                add(new FileExtension("foo..c"));
+                add(new FileExtension("fooc"));
+                // Just dots
+                add(new FileExtension("."));
+                add(new FileExtension(".."));
+                add(new FileExtension("...c"));
+                add(new FileExtension(".pl"));
+                // Slashes
+                add(new FileExtension("/"));
+                add(new FileExtension("/."));
+                add(new FileExtension("///"));
+                add(new FileExtension("///./py"));
+                // Misleading directory name
+                add(new FileExtension("///.c/py"));
+                add(new FileExtension("///./.py"));
+                add(new FileExtension("///.el"));
+                add(new FileExtension("///./foo.py"));
+                add(new FileExtension("///foo.xml"));
+                // Repeat with fewer empty path components
+                add(new FileExtension("/foo/bar.pl"));
+                add(new FileExtension("/foo/bar.py/baz.c"));
+                add(new FileExtension("/foo/bar.py/"));
+                // Check it's null-safe
+                add(new FileExtension(null));
+                // Look for different file types
+                add(new FileExtension("FileList.java"));
+                add(new FileExtension("FileList.xml"));
+                add(new FileExtension("FileList.docbook"));
+                add(new FileExtension("FileList.tex"));
+                add(new FileExtension("FileList.xbm"));
+                add(new FileExtension("FileList.png"));
+                add(new FileExtension("FileList.po"));
+                // And some .po files
+                add(new FileExtension("/foo/en_GB.properties.c"));
+                add(new FileExtension("/foo/en_GB.properties"));
+                add(new FileExtension("/foo/en_GB.c.po"));
        	    }
         };
     }
@@ -34,7 +82,11 @@ public class FileType
     {
         public FileExtension(String name)
         {
-            this.name = name;
+        	if (null == name) {
+        		this.name = "(null)";
+        	} else {
+        		this.name = name;
+        	}
             this.extension = FileTypeMatcher.getFileExtension(name);
             FileTypeMatcher.FileType ft = FileTypeMatcher.getFileType(name);
             if (null == this.extension) {
