54b3dc82ddc79f95bc37f19470a212c96042c030
Adriaan de Groot <groot@kde.org>
2008-09-18 11:12:25 +0000

    Reintroduce the static methods removed earlier; fix copyright line. For the static methods, Romain has a handy rule-of-thumb: if it returns V and is code that could have been written anywhere that futzes with the DB, put it in class V.

--- alitheia/core/src/eu/sqooss/service/db/ProjectVersion.java
+++ alitheia/core/src/eu/sqooss/service/db/ProjectVersion.java
@@ -2,7 +2,8 @@
  * This file is part of the Alitheia system, developed by the SQO-OSS
  * consortium as part of the IST FP6 SQO-OSS project, number 033331.
  *
- * Copyright 2007-2008 by the SQO-OSS consortium members <info@sqo-oss.eu>
+ * Copyright 2007,2008 Athens University of Economics and Business
+ *     Author Adriaan de Groot <groot@kde.org>
  * Copyright 2007-2008 by Paul J. Adams <paul.adams@siriusit.co.uk>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -235,6 +236,17 @@ public class ProjectVersion extends DAObject {
     }
 
     /**
+     * Less-than-or-equal (operator <=) for project versions, based
+     * on timestamps. The compared version must not be null.
+     * 
+     * @param p comparison version
+     * @return true if this <= p, in terms of timestamps
+     */
+    public boolean lte(ProjectVersion p) {
+        return this.timestamp <= p.getTimestamp();
+    }
+    
+    /**
      * Allow moving backward in version history by finding the most-recent
      * version of this project before the current one, or null if there
      * is no such version.
@@ -303,30 +315,98 @@ public class ProjectVersion extends DAObject {
     }
 
     /**
-     * Less-than-or-equal (operator <=) for project versions, based
-     * on timestamps. The compared version must not be null.
+     * Look up a project version for this project based on the SVN
+     * revision number (e.g. 1). This does a database lookup and 
+     * returns the ProjectVersion recorded for that SVN revision,
+     * or null if there is no such revision (for instance because
+     * the updater has not added it yet or the revision number is
+     * invalid in some way). This is a lookup, not a creation, of
+     * revisions.
      * 
-     * @param p comparison version
-     * @return true if this <= p, in terms of timestamps
+     * @param project Project to look up
+     * @param revision SVN revision number to look up for this project
+     * @return ProjectVersion object corresponding to the revision,
+     *         or null if there is none.
      */
-    public boolean lte(ProjectVersion p) {
-        return this.timestamp <= p.getTimestamp();
+    public static ProjectVersion getVersionByRevision(StoredProject project, long revision) {
+        DBService dbs = AlitheiaCore.getInstance().getDBService();
+   
+        Map<String,Object> parameters = new HashMap<String,Object>();
+        parameters.put("project", project);
+        parameters.put("version", revision);
+
+        List<ProjectVersion> versions = dbs.findObjectsByProperties(ProjectVersion.class, parameters);
+        if (versions == null || versions.size() == 0) {
+            return null;
+        } else {
+            return versions.get(0);
+        }
     }
     
     /**
-     * Pseudo-constructor to look up a stored ProjectVersion by project
-     * and revision -- this is different from creating a new object
-     * through the constructor because it will return null if the combination
-     * of project and SVN revision does not exist in the database.
-     * 
+     * Convenience method, essentially like the above.
      * @param project Project to look up
-     * @param revision SVN revision (must have a revision number associated)
-     * @return ProjectVersion, or null if none found
+     * @param revision ProjectRevision to use for the lookup.
+     * @return ProjectVersion corresponding to the SVN revision, or
+     *          null if there is none.
      */
-    public static ProjectVersion getVersionByRevision( StoredProject project, ProjectRevision revision ) {
-        return project.getVersionByRevision(revision);
+    public static ProjectVersion getVersionByRevision(StoredProject project, ProjectRevision revision) {
+        if (!revision.hasSVNRevision()) {
+            // Can't do the lookup if we don't have a SVN revision number;
+            // caller should have resolved other revision specifications 
+            // to a number already.
+            return null;
+            // TODO: possibly log this
+        }
+        return getVersionByRevision(project, revision.getSVNRevision());
     }
     
+    /**
+     * Convenience method to find the latest project version for
+     * a given project.
+     * 
+     * @return ProjectVersion for the latest version or null if not found
+     */
+    public static ProjectVersion getLastProjectVersion(StoredProject sp) {
+        DBService dbs = AlitheiaCore.getInstance().getDBService();
+
+        Map<String,Object> parameterMap = new HashMap<String,Object>();
+        parameterMap.put("sp", sp);
+        List<?> pvList = dbs.doHQL("from ProjectVersion pv where pv.project=:sp"
+                + " and pv.version = (select max(pv2.version) from "
+                + " ProjectVersion pv2 where pv2.project=:sp)",
+                parameterMap);
+
+        return (pvList == null || pvList.isEmpty()) ? null : (ProjectVersion) pvList.get(0);
+    }
+
+    /**
+     * For a given metric and project, return the latest version of that
+     * project that was actually measured.  If no measurements have been made, 
+     * it returns null. For the returned revision which is not null, the
+     * revision is greater than 0, there is a measurement in the database.
+     * 
+     * @param m Metric to look for
+     * @param p Project to look for
+     * @return Last version measured, or revision 0.
+     */
+    public static ProjectVersion getLastMeasuredVersion(Metric m, StoredProject p) {
+        String query = "select pvm from ProjectVersionMeasurement pvm, ProjectVersion pv" +
+           " where pvm.projectVersion = pv" +
+           " and pvm.metric = :metric and pv.project = :project" +
+           " order by pv.timestamp desc";
+
+        HashMap<String, Object> params = new HashMap<String, Object>(4);
+        params.put("metric", m);
+        params.put("project", p);
+        List<?> pvmList = AlitheiaCore.getInstance().getDBService().doHQL( query, params, 1);
+	    
+        ProjectVersion previous = pvmList.isEmpty() ? 
+                null :
+                ((ProjectVersionMeasurement) pvmList.get(0)).getProjectVersion();
+        return previous;
+    }
+
     /**
      * Gets the number of files in the given version which are in the
      * selected file state.
@@ -356,7 +436,7 @@ public class ProjectVersion extends DAObject {
         // Default result
         return 0;
     }
-    
+
     public String toString() {
         return "ProjectVersion(\"" + this.project.getName() + "\",r" + this.getVersion() +")";
     }
