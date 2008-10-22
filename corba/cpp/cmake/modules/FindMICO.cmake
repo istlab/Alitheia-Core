aca34668a754d345bb43eeb8658aac21dda208fc
Christoph Schleifenbaum <christoph@kdab.net>
2008-10-22 12:48:44 +0000

    Fix out ouf source builds

--- corba/cpp/cmake/modules/FindMICO.cmake
+++ corba/cpp/cmake/modules/FindMICO.cmake
@@ -1,5 +1,9 @@
 # Finds MICO libraries
 
+if( MICO_INCLUDE_DIR AND MICO_LIBRARIES )
+    set( MICO_FIND_QUIETLY TRUE )
+endif( MICO_INCLUDE_DIR AND MICO_LIBRARIES )
+
 find_package( OpenSSL REQUIRED )
 
 find_path( MICO_INCLUDE_DIR mico/config.h )
@@ -10,7 +14,25 @@ find_library( MICO_LIBRARY NAMES mico2.3.12
 find_library( MICOCOSS_LIBRARY NAMES micocoss2.3.12
                                      micocoss2.3.13 )
 
+find_program( MICO_IDL_COMPILER idl )
+
 set( MICO_LIBRARIES ${MICO_LIBRARY} 
                     ${MICOCOSS_LIBRARY} 
                     ${OPENSSL_LIBRARIES}
      CACHE LIST "MICO libraries" )
+
+if( MICO_INCLUDE_DIR AND MICO_LIBRARY AND MICOCOSS_LIBRARY )
+    set( MICO_FOUND TRUE )
+else( MICO_INCLUDE_DIR AND MICO_LIBRARY AND MICOCOSS_LIBRARY )
+    set( MICO_FOUND FALSE )
+endif( MICO_INCLUDE_DIR AND MICO_LIBRARY AND MICOCOSS_LIBRARY )
+
+if( MICO_FOUND )
+    if( NOT MICO_FIND_QUIETLY )
+        message( STATUS "Found MICO: ${MICO_LIBRARY} ${MICOCOSS_LIBRARY}" )
+    endif( NOT MICO_FIND_QUIETLY )
+else( MICO_FOUND )
+    if( MICO_FIND_REQUIRED )
+        message( FATAL_ERROR "Could NOT find MICO" )
+    endif( MICO_FIND_REQUIRED )
+endif( MICO_FOUND )
