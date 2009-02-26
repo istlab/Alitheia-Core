af03fe1614e6fd96c3086e445fa9c7e8eb29396b
Georgios Gousios <gousiosg@aueb.gr>
2009-02-26 16:09:49 +0000

    Heuristic-based match maker of usernames to emails-realnames. Avoids fancy heuristics (metaphone, levenshtein distance) for now.

--- alitheia/core/src/eu/sqooss/impl/service/updater/DeveloperMatcher.java
+++ alitheia/core/src/eu/sqooss/impl/service/updater/DeveloperMatcher.java
@@ -32,6 +32,7 @@
  */
 package eu.sqooss.impl.service.updater;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -41,24 +42,31 @@ import eu.sqooss.core.AlitheiaCore;
 import eu.sqooss.service.db.DBService;
 import eu.sqooss.service.db.Developer;
 import eu.sqooss.service.db.DeveloperAlias;
-import eu.sqooss.service.db.StoredProject;
 import eu.sqooss.service.scheduler.Job;
+import eu.sqooss.service.util.Pair;
+
+import org.apache.commons.codec.language.DoubleMetaphone;
 
 /**
- * Heuristic based matcher for developer identities. Will lock all developer
- * records per project to avoid concurrent access when running.
+ * Heuristic based matcher for developer identities. Uses a combination of 
+ * pattern and approximate string matching techniques and weights that are
+ * evaluated at the end of the process. Will lock all developer
+ * records per project to avoid concurrent access when running. 
  * 
  * @author Georgios Gousios <gousiosg@gmail.com>
  */
 public class DeveloperMatcher extends UpdaterBaseJob {
 
-    private StoredProject sp;
     private DBService dbs;
     
     private Map<String, Developer> emailToDev = new TreeMap<String, Developer>();
     private Map<String, Developer> unameToDev = new TreeMap<String, Developer>();
     private Map<String, Developer> nameToDev = new TreeMap<String, Developer>();
     private Map<String, Developer> emailprefToDev = new TreeMap<String, Developer>();
+    
+    private Map<String, List<String>> mtphoneToUname = new TreeMap<String, List<String>>();
+    
+    private Map<Pair<Long, Long>, Integer> matches = new HashMap<Pair<Long, Long>, Integer>();
     
     public DeveloperMatcher() {
         dbs = AlitheiaCore.getInstance().getDBService();
@@ -72,61 +80,323 @@ public class DeveloperMatcher extends UpdaterBaseJob {
     @Override
     protected void run() throws Exception {
         dbs.startDBSession();
-        
-        String query = "from Developer d where d.storedProject = :sp" ;
+        project = dbs.attachObjectToDBSession(project);
+        DoubleMetaphone dm = new DoubleMetaphone();
         Map<String, Object> params = new HashMap<String, Object>();
-        params.put("sp", sp);
-        List<Developer> devs = (List<Developer>) dbs.doHQL(query, params, true);
-        
+        params.put("storedProject", project);
+        List<Developer> devs = dbs.findObjectsByPropertiesForUpdate(Developer.class, params);
+        long ts = System.currentTimeMillis();
         //Fill in indices
         for (Developer d : devs) {
             for (DeveloperAlias da : d.getAliases()) {
                 emailToDev.put(da.getEmail(), d);
-                emailprefToDev.put(da.getEmail().substring(0, da.getEmail().indexOf('@')).toLowerCase(), d);
+                String uname = da.getEmail().substring(0, da.getEmail().indexOf('@')).toLowerCase();
+                emailprefToDev.put(uname, d);
+                addMetaphone(dm.doubleMetaphone(uname), uname);            
             }
             
-            if (d.getName() != null) {
-                unameToDev.put(d.getUsername().toLowerCase(), d);
+            if (d.getUsername() != null && !d.getUsername().equals("")) {
+                String uname = d.getUsername().toLowerCase();
+                unameToDev.put(uname, d);
+                addMetaphone(dm.doubleMetaphone(uname), uname);
             }
             
-            if (d.getName() != null) {
+            if (d.getName() != null && !d.getName().equals("")) {
                 nameToDev.put(d.getName().toLowerCase(), d);
             }
         }
         
-        for (Developer d : devs) {
+        for (String name : nameToDev.keySet()) {
+            List<String> usernames = getPossibleUnames(name);
+            for (String uname : usernames) {
+                // Try strict matching first
+                if (unameToDev.containsKey(uname)) {
+                    addMatch(nameToDev.get(name).getId(), 
+                            unameToDev.get(uname).getId(), 10);
+                }
+
+              /*  String mph = dm.doubleMetaphone(uname);
+
+                // Try metaphone matching
+                if (mtphoneToUname.containsKey(mph)) {
+                    List<String> mfMatches = mtphoneToUname.get(mph);
+                    for (String mfMatch : mfMatches) {
+                        //Match against user names list first
+                        Developer d = unameToDev.get(mfMatch);
+                        if (d != null) {
+                            addMatch(nameToDev.get(name).getId(),
+                                    d.getId(), 
+                                    10 - levenshtein(mfMatch, uname));    
+                        } else {
+                            //Match against email prefixes
+                            d = emailprefToDev.get(mfMatch);
+                            addMatch(nameToDev.get(name).getId(),
+                                    d.getId(),  
+                                    10 - levenshtein(mfMatch, uname)); 
+                        }
+                        
+                    }
+                }
+             
+                // Try levenshtein distance matching
+                for (String develuname : unameToDev.keySet()) {
+                    int dist = Math.abs(levenshtein(uname, develuname));
+                    int mlen = Math.abs(uname.length() - develuname.length()); 
+                    //We arbitrarily consider a diff > 2 too big
+                    if (mlen <= 2 && dist <= 2) {
+                        addMatch(nameToDev.get(name).getId(),
+                                unameToDev.get(develuname).getId(), 
+                                2 - dist);
+                    }
+                }*/
+            }
+        }
+        
+        List<String> updates = new ArrayList<String>(); 
+        updates.add("update ProjectVersion set committer = :new where committer = :old");
+        updates.add("update MailMessage set sender = :new where sender = :old");
+        updates.add("update Bug set reporter = :new where reporter = :old");
+        updates.add("update BugReportMessage set reporter = :new where reporter = :old");
+
+        List<String> deletes = new ArrayList<String>(); 
+        deletes.add("delete from DeveloperAlias d where d.developer.id = :oldid");
+        deletes.add("delete from Developer d where d.id = :oldid");
+        
+        
+        for (Pair<Long, Long> match : matches.keySet()) {
+            Developer byName = Developer.loadDAObyId(match.first, Developer.class);
+            Developer byUsrName = Developer.loadDAObyId(match.second, Developer.class);
             
-            //Developer is registered by username 
-            if (d.getAliases().isEmpty()) {
-                
+            Map<String, Object> updParam = new HashMap<String, Object>();
+            updParam.put("old", byName);
+            updParam.put("new", byUsrName);
+            
+            Map<String, Object> delParam = new HashMap<String, Object>();
+            delParam.put("oldid", byName.getId());
+            
+            //Copy emails
+            for (DeveloperAlias da : byName.getAliases()) {
+                debug("Adding alias " + da.getEmail() + " to dev " + byUsrName);
+                byUsrName.addAlias(da.getEmail());
             }
             
-            //We have the developer's name, check if it matches a user name
-            if (d.getName() != null || d.getName().length() > 0) {
-                String[] nameParts = d.getName().split(" ");
-                int namePartNo = nameParts.length;
-                
-                //name.surname@email.com
-                if (emailprefToDev.containsKey(nameParts[0] + "." + nameParts[namePartNo - 1])) {
-                    
-                }
-                
-                //nsurname@email.com
-                if (emailprefToDev.containsKey(nameParts[namePartNo - 1] + "." + nameParts[0])) {
-                    
-                }
-                
-                //nsurname@email.com
-                if (emailprefToDev.containsKey(nameParts[namePartNo - 1] + "." + nameParts[0])) {
-                    
-                }
-                
+            if (byUsrName.getName() == null || byUsrName.getName().trim().equals("")) {
+                byUsrName.setName(byName.getName());
+                debug("Setting " +  byUsrName.getUsername() + "'s name to " + byName.getName());
             }
+            
+            for (String upd : updates) {
+                dbs.executeUpdate(upd, updParam);
+            }
+            
+            for (String del : deletes) {
+                dbs.executeUpdate(del, delParam);
+            }
+            
+            debug("Replaced dev " + match.first + "->" 
+                    + match.second + ", score: " + matches.get(match));
+        }
+        
+        info("Matched " + matches.size() + " developers in " 
+                + (System.currentTimeMillis() - ts) + "ms");
+        //dbs.commitDBSession();
+    }
+    
+    /*
+     * Get a list of possible usernames that may originate from 
+     * a given real name
+     */
+    private List<String> getPossibleUnames(String realName) {
+        List<String> names = new ArrayList<String>();
+        realName = cleanup(realName);
+        
+        String[] nameParts = realName.split(" ");
+        String fname = nameParts[0];
+        names.add(fname);
+        
+        String surname = null;
+        if (nameParts.length > 0) {
+            surname = nameParts[nameParts.length - 1];
+        }
+        
+        String mname = null;
+        if (nameParts.length == 3) {
+            mname = nameParts[1];
+        }
+        
+        //Not a lot of usernames possible without a surname 
+        if (surname == null) 
+            return names;
+        
+        //fnamesurname
+        names.add(fname + surname);
+        //fname.surname
+        names.add(fname + "." + surname);
+        //surname.fname
+        names.add(surname + "." + fname);
+        //sfname
+        names.add(surname.charAt(0) + fname);
+        //fsurname
+        names.add(fname.charAt(0) + surname);
+        //surnamef
+        names.add(surname + fname.charAt(0));
+        //fnames
+        names.add(fname + surname.charAt(0));
+        //f.surname
+        names.add(fname.charAt(0) + "." + surname);
+        //fname.s
+        names.add(fname + "." + surname.charAt(0));
+        
+        if (mname != null) {
+            //Traditional Unix 
+            names.add("" + fname.charAt(0) + mname.charAt(0) + surname.charAt(0));
+        }
+        
+        return names;
+    }
+    
+    /*
+     * Names coming from email headers might contain various characters
+     * which are not part of a real name. Try to filter those out. 
+     */
+    private String cleanup(String name) {
+        String badCharsRE = "\\/|\\|\'|\"|!";
+
+        name.replaceAll(badCharsRE, name);
+        return name;
+    }
+    
+    /*
+     * Add a match to the matches table. If the match already exists,
+     * just increase the match score.
+     */
+    private void addMatch(Long id1, Long id2, Integer score) {
+        
+        if (id1.longValue() == id2.longValue())
+            return;
+        
+        Pair<Long, Long> match = new Pair<Long, Long>(id1, id2);
+        Pair<Long, Long> found = null;
+        for (Pair<Long, Long> p : matches.keySet()) {
+            if ((p.first.longValue() == id1.longValue()) 
+                    && (p.second.longValue() == id2.longValue()) ||
+                    (p.second.longValue() == id1.longValue()) 
+                    && (p.first.longValue() == id2.longValue())) {
+                found = p;
+                break;
+            }
+        }
+        
+        if (found != null) {
+            matches.put(found, matches.get(found) + score);
+        } else {
+            matches.put(match, score);
+        }
+        
+        debug("Potential developer match " + id1 + "->" + id2 + ": " 
+                + score);
+    }
+    
+    /*
+     * Add a value to the metaphone cache, taking care duplicate keys
+     */
+    private void addMetaphone(String code, String username) {
+        List<String> unames = mtphoneToUname.get(code);
+        
+        if (unames != null) {
+            if (!unames.contains(username)) {
+                unames.add(username);
+            }
+        } else {
+            unames = new ArrayList<String>();
+            unames.add(username);
+            mtphoneToUname.put(code, unames);
         }
     }
 
+    /*
+     * Levenshtein distance algorithm, copied verbatim from
+     * http://www.merriampark.com/ld.htm
+     */
+    public int levenshtein(String s, String t) {
+        int d[][]; // matrix
+        int n; // length of s
+        int m; // length of t
+        int i; // iterates through s
+        int j; // iterates through t
+        char s_i; // ith character of s
+        char t_j; // jth character of t
+        int cost; // cost
+
+        // Step 1
+        n = s.length();
+        m = t.length();
+        if (n == 0) {
+            return m;
+        }
+        
+        if (m == 0) {
+            return n;
+        }
+        
+        d = new int[n + 1][m + 1];
+
+        // Step 2
+        for (i = 0; i <= n; i++) {
+            d[i][0] = i;
+        }
+
+        for (j = 0; j <= m; j++) {
+            d[0][j] = j;
+        }
+
+        // Step 3
+        for (i = 1; i <= n; i++) {
+            s_i = s.charAt(i - 1);
+
+            // Step 4
+            for (j = 1; j <= m; j++) {
+
+                t_j = t.charAt(j - 1);
+
+                // Step 5
+                if (s_i == t_j) {
+                    cost = 0;
+                } else {
+                    cost = 1;
+                }
+
+                // Step 6
+                d[i][j] = min(d[i - 1][j] + 1, d[i][j - 1] + 1,
+                        d[i - 1][j - 1] + cost);
+            }
+        }
+
+        // Step 7
+        return d[n][m];
+    }
+
+    private int min(int a, int b, int c) {
+        int mi;
+
+        mi = a;
+        if (b < mi) {
+            mi = b;
+        }
+        if (c < mi) {
+            mi = c;
+        }
+        return mi;
+
+    }
+    
     @Override
     public Job getJob() {
         return this;
+    }
+    
+    @Override
+    public String toString() {
+        return "Developer Updater - Project:" + project;
     }
 }
