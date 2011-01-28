656aaada0b1d85c3efd5fe6935d8e203fa346009
Georgios Gousios <gousiosg@gmail.com>
2011-01-28 14:36:25 +0200

    Order config fields lexicographically

--- alitheia/core/src/main/java/eu/sqooss/service/db/ConfigOption.java
+++ alitheia/core/src/main/java/eu/sqooss/service/db/ConfigOption.java
@@ -9,20 +9,45 @@ package eu.sqooss.service.db;
  */
 public enum ConfigOption {
     
-    /**
-     * The project's name
+	/**
+     * The project's original BTS URL
      */
-    PROJECT_NAME("eu.sqooss.project.name", "The project's name"),
+    PROJECT_BTS_SOURCE("eu.sqooss.project.bts.source", "The project's original BTS URL"),
     
     /**
-     * The project's website
+     * The project's BTS type
      */
-    PROJECT_WEBSITE("eu.sqooss.project.website", "The project's website"),
+    PROJECT_BTS_TYPE("eu.sqooss.project.bts.type", "The project's BTS type"),
+	
+    /**
+     * The project's BTS URL
+     */
+    PROJECT_BTS_URL("eu.sqooss.project.bts.url", "The project's local BTS mirror URL"),
     
     /**
      * The project's contact address
      */
     PROJECT_CONTACT("eu.sqooss.project.contact", "The project's contact address"),
+    
+    /**
+     * The project's MailingList source URL
+     */
+    PROJECT_ML_SOURCE("eu.sqooss.project.ml.source", "The project's source mailing list URL"),
+    
+    /**
+     * The project's MailingList source URL
+     */
+    PROJECT_ML_TYPE("eu.sqooss.project.ml.type", "The project's local mailing list type"),
+
+    /**
+     * The project's MailingList URL
+     */
+    PROJECT_ML_URL("eu.sqooss.project.ml.url", "The project's local mailing list URL"),
+    
+    /**
+     * The project's name
+     */
+    PROJECT_NAME("eu.sqooss.project.name", "The project's name"),
     
     /**
      * The project's SCM type
@@ -40,36 +65,6 @@ public enum ConfigOption {
     PROJECT_SCM_SOURCE("eu.sqooss.project.scm.source", "The project's original SCM URL"),
     
     /**
-     * The project's MailingList URL
-     */
-    PROJECT_ML_URL("eu.sqooss.project.ml.url", "The project's local mailing list URL"),
-    
-    /**
-     * The project's MailingList source URL
-     */
-    PROJECT_ML_SOURCE("eu.sqooss.project.ml.source", "The project's source mailing list URL"),
-    
-    /**
-     * The project's MailingList source URL
-     */
-    PROJECT_ML_TYPE("eu.sqooss.project.ml.type", "The project's local mailing list type"),
-    
-    /**
-     * The project's BTS URL
-     */
-    PROJECT_BTS_URL("eu.sqooss.project.bts.url", "The project's local BTS mirror URL"),
-    
-    /**
-     * The project's BTS URL
-     */
-    PROJECT_BTS_SOURCE("eu.sqooss.project.bts.source", "The project's original BTS URL"),
-    
-    /**
-     * The project's BTS type
-     */
-    PROJECT_BTS_TYPE("eu.sqooss.project.bts.type", "The project's BTS type"),
-    
-    /**
      * The source code paths to process while executing the updater
      */
     PROJECT_SCM_PATHS_INCL("eu.sqooss.project.scm.path.incl", "The source code paths to process"),
@@ -77,7 +72,13 @@ public enum ConfigOption {
     /**
      * The source code paths to process not to process
      */
-    PROJECT_SCM_PATHS_EXCL("eu.sqooss.project.scm.path.excl", "The source code paths not to process");
+    PROJECT_SCM_PATHS_EXCL("eu.sqooss.project.scm.path.excl", "The source code paths not to process"),
+    
+    /**
+     * The project's website
+     */
+    PROJECT_WEBSITE("eu.sqooss.project.website", "The project's website");
+    
     
     private final String propname;
     private final String desc;
