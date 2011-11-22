27efdaef546379909e6667c36712cdfb20c02c62
Georgios Gousios <gousiosg@gmail.com>
2011-11-22 13:08:54 +0200

    Remove obsolete invocation rule support

--- alitheia/core/src/main/java/eu/sqooss/impl/service/metricactivator/MetricActivatorImpl.java
+++ alitheia/core/src/main/java/eu/sqooss/impl/service/metricactivator/MetricActivatorImpl.java
@@ -46,7 +46,6 @@ import java.util.TreeSet;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
 
 import eu.sqooss.core.AlitheiaCore;
 import eu.sqooss.service.abstractmetric.AbstractMetric;
@@ -154,102 +153,7 @@ public class MetricActivatorImpl  implements MetricActivator {
     }
 
     public ActionType matchRules (AlitheiaPlugin ap, DAObject resource) {
-        // Load the rules chain, if not done yet
-        if (rules == null)
-            initRules();
-
-        // Retrieve the first rule
-        InvocationRule rule = rules.get(firstRuleId);
-
-        // Retrieve the plug-in DAO
-        Plugin plugin = null;
-        if ((ap != null) && (ap.getUniqueKey() != null)) {
-            HashMap<String, Object> properties =
-                new HashMap<String, Object>();
-            List<Plugin> plugins =
-                db.findObjectsByProperties(Plugin.class, properties);
-            if ((plugins != null) && (plugins.size() > 0)) {
-                plugin = plugins.get(0);
-            }
-        }
-        //====================================================================
-        // Match against a resource of type ProjectFile
-        //====================================================================
-        if (resource instanceof ProjectFile) {
-            // Skip on project file in state "DELETED"
-            if (((ProjectFile) resource).getState().equals(ProjectFileState.deleted())) {
-                return defaultAction;
-            }
-            ProjectVersion version =
-                ((ProjectFile) resource).getProjectVersion();
-            StoredProject project = version.getProject();
-            // Check for an invalid resource parameters
-            if ((project == null)
-                    || (plugin == null)
-                    || (version == null)) {
-                return defaultAction;
-            }
-            // Traverse through each rule until a match is found.
-            while (rule != null) {
-                // Check for the default rule
-                if (rule.getId() == defaultRuleId) {
-                    return defaultAction;
-                }
-                // Retrieve the rule's metric type
-                Type metricType = null;
-                if (rule.getMetricType() != null) {
-                    metricType = (db.attachObjectToDBSession(
-                            rule.getMetricType())).getEnumType();
-                }
-                // Skip on a metric type which can not evaluate that resource
-                if ((metricType != null)
-                        && (metricType != Type.SOURCE_FILE)
-                        && (metricType != Type.SOURCE_DIRECTORY)
-                        && (metricType != Type.PROJECT_VERSION)){
-                    rule = rules.get(rule.getNextRule());
-                    continue;
-                }
-                // Skip on a different project Id
-                if ((rule.getProject() != null)
-                        && (rule.getProject().getId() != project.getId())) {
-                    rule = rules.get(rule.getNextRule());
-                    continue;
-                }
-                // Skip on a different plug-in Id
-                if ((rule.getPlugin() != null)
-                        && (rule.getPlugin().getId() != plugin.getId())) {
-                    rule = rules.get(rule.getNextRule());
-                    continue;
-                }
-                // Skip on a different metric type
-                boolean metricTypeNotFound = true;
-                for (Metric metric : ap.getAllSupportedMetrics()) {
-                    metric = db.attachObjectToDBSession(metric);
-                    if (metric.getMetricType().getEnumType() == metricType) {
-                        metricTypeNotFound = false;
-                    }
-                }
-                if ((metricType != null) && (metricTypeNotFound)) {
-                    rule = rules.get(rule.getNextRule());
-                    continue;
-                }
-                // Match against the current rule
-                if (rule.match(
-                        ScopeType.fromString(rule.getScope()),
-                        rule.getValue(),
-                        (ProjectFile) resource)) {
-                    // TODO: Remove the debug to speed up processing
-                    logger.debug("Rule match: "
-                            + ((ProjectFile) resource).getFileName()
-                            + " : " + rule.getAction());
-                    return ActionType.fromString(rule.getAction());
-                }
-                // Move to the next rule
-                rule = rules.get(rule.getNextRule());
-            }
-        }
-        // No matching rule found. Return the default action.
-        return defaultAction;
+        return ActionType.EVAL;
     }
 
     @Override
