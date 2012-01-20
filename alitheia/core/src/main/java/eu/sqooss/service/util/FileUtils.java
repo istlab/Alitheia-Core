2c348d7dc112c11c578047a8c37074f9a4c0e377
Georgios Gousios <gousiosg@gmail.com>
2012-01-20 20:44:58 +0200

    Method to copy files

--- alitheia/core/src/main/java/eu/sqooss/service/util/FileUtils.java
+++ alitheia/core/src/main/java/eu/sqooss/service/util/FileUtils.java
@@ -33,8 +33,7 @@
 
 package eu.sqooss.service.util;
 
-import java.io.File;
-import java.io.ByteArrayOutputStream;
+import java.io.*;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -269,6 +268,33 @@ public class FileUtils {
     public enum FindOpt {
         FILES, DIRS, ALL
     }
+
+    public static void copyFile(File source, File dest) throws IOException {
+        if (!dest.exists()) {
+            dest.createNewFile();
+        }
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            in = new FileInputStream(source);
+            out = new FileOutputStream(dest);
+
+            // Transfer bytes from in to out
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = in.read(buf)) > 0) {
+                out.write(buf, 0, len);
+            }
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
+
 }
 
 // vi: ai nosi sw=4 ts=4 expandtab
