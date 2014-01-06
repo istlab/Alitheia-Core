f54e52f59d30f9f59d5a7892c369e6b6159eecaa
Elwin <elwindokter@gmail.com>
2014-01-06 14:27:33 +0100

    Renamed test package

---
+++ alitheia/core/src/test/java/eu/sqooss/test/service/webadmin/TestAbstractView.java
@@ -0,0 +1,47 @@
+package eu.sqooss.test.service.webadmin;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Locale;
+
+import org.apache.velocity.VelocityContext;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.osgi.framework.BundleContext;
+
+import eu.sqooss.core.AlitheiaCore;
+import eu.sqooss.impl.service.webadmin.AbstractView;
+import eu.sqooss.impl.service.webadmin.PluginsView;
+
+public class TestAbstractView {
+
+	AbstractView abstractView;
+	BundleContext bundleContext;
+	VelocityContext velocityContext;
+	AlitheiaCore alitheiaCore;
+	
+	@Before
+	public void setUp() throws Exception {
+		bundleContext = mock(BundleContext.class);
+		velocityContext = mock(VelocityContext.class);
+		alitheiaCore = mock(AlitheiaCore.class);
+		assertNotNull(alitheiaCore);
+		abstractView = new PluginsView(bundleContext,velocityContext);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		bundleContext = null;
+		velocityContext = null;
+		abstractView = null;
+	}
+
+	@Test
+	public void testInitResources() {
+		Locale locale = mock(Locale.class);
+		AbstractView.initResources(locale);
+	}
+
+}
