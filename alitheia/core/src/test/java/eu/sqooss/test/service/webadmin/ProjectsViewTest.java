8a2bb7734a858a181b9ea4968f7675b587d358e0
Sijmen <sijmen.hoogendijk@gmail.com>
2014-01-08 18:07:04 +0100

    First test setup for ProjectsView

--- alitheia/core/src/test/java/eu/sqooss/test/service/webadmin/ProjectsViewTest.java
+++ alitheia/core/src/test/java/eu/sqooss/test/service/webadmin/ProjectsViewTest.java
@@ -8,11 +8,18 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import eu.sqooss.impl.service.webadmin.ProjectsView;
 
 /**
  * @author elwin
  *
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(ProjectsView.class)
 public class ProjectsViewTest {
 
 	/**
