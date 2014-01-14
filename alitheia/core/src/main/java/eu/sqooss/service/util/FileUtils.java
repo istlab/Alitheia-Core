c3243d4cb06842dc41434fa4ec5a6757b31b3d43
Jasper Ruoff <jruoff@gmail.com>
2014-01-14 16:45:56 +0100

    Remove dead code from FileUtils

--- alitheia/core/src/main/java/eu/sqooss/service/util/FileUtils.java
+++ alitheia/core/src/main/java/eu/sqooss/service/util/FileUtils.java
@@ -33,8 +33,9 @@
 
 package eu.sqooss.service.util;
 
-import java.io.*;
-import java.util.*;
+import java.io.File;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -43,64 +44,11 @@ import java.util.regex.Pattern;
  */
 public class FileUtils {
     /**
-     * Read the contents of a file and return them as a byte array.
-     *
-     * @param f File to read.
-     * @return File contents as a byte array or null on error (such
-     *      as empty file, no such file, or IO error).
-     */
-    public static byte[] fileContents(File f) {
-        if (f==null) {
-            // Bad parameters
-            return null;
-        }
-
-        if (!(f.exists() && f.isFile() && f.canRead())) {
-            // Can only handle existing files
-            return null;
-        }
-
-        int ilength = 0;
-        // Block for hiding the long variable length.
-        try {
-            long length = f.length();
-            if (length > Integer.MAX_VALUE) {
-                // Refuse to return more than a few GB of data
-                return null;
-            }
-            ilength = (int)length;
-        } finally {
-            if (ilength < 1) {
-                return null;
-            }
-        }
-
-        try {
-            java.io.InputStream i = new java.io.FileInputStream(f);
-            ByteArrayOutputStream o = new ByteArrayOutputStream(ilength);
-
-            // Read in chunks at a time.
-            // TODO: optimize this away and create one byte array of the
-            // right length already.
-            byte[] chunk = new byte[(ilength > 16384) ? 16384 : ilength];
-            int r;
-            while ( (r=i.read(chunk,0,chunk.length)) >= 0) {
-                o.write(chunk,0,r);
-            }
-
-            return o.toByteArray();
-        } catch (java.io.IOException e) {
-            // Just give up.
-            return null;
-        }
-    }
-    
-    /**
      * Return the filename portion of a path. 
      * @param path The path to examine
      * @return The filename or empty string if the path is empty or null
      */
-    public static String basename (String path) {
+    public static String basename(String path) {
         String filename = path.substring(path.lastIndexOf('/') + 1);
         
         if (filename == null || filename.equalsIgnoreCase("")) {
@@ -228,73 +176,6 @@ public class FileUtils {
             return findBreadthFirst(dir, p);
         return null;
     }
-
-    public static List<File> findDirs(File path) {
-        return find(path, FindOpt.DIRS);
-    }
-
-    public static List<File> findFiles(File path) {
-        return find(path, FindOpt.FILES);
-    }
-
-    public static List<File> find(File path, FindOpt what) {
-        Set<File> toReturn = new HashSet<File>();
-
-        boolean dirs = (what == FindOpt.DIRS || what == FindOpt.ALL);
-        boolean files = (what == FindOpt.FILES || what == FindOpt.ALL);
-
-        File[] c = path.listFiles();
-        for (File file : c) {
-            if (file.isFile() && files)
-                toReturn.add(file);
-
-            if (file.isDirectory() && dirs) {
-                toReturn.add(file);
-                toReturn.addAll(find(file, what));
-            }
-        }
-
-        List<File> result = new ArrayList<File>(toReturn);
-
-        Collections.sort(result, new Comparator<File>() {
-            @Override
-            public int compare(File a, File b) {
-                return (a.getAbsolutePath().compareTo(b.getAbsolutePath()));
-            }
-        });
-        return result;
-    }
-
-    public enum FindOpt {
-        FILES, DIRS, ALL
-    }
-
-    public static void copyFile(File source, File dest) throws IOException {
-        if (!dest.exists()) {
-            dest.createNewFile();
-        }
-        InputStream in = null;
-        OutputStream out = null;
-        try {
-            in = new FileInputStream(source);
-            out = new FileOutputStream(dest);
-
-            // Transfer bytes from in to out
-            byte[] buf = new byte[1024];
-            int len;
-            while ((len = in.read(buf)) > 0) {
-                out.write(buf, 0, len);
-            }
-        } finally {
-            if (in != null) {
-                in.close();
-            }
-            if (out != null) {
-                out.close();
-            }
-        }
-    }
-
 }
 
 // vi: ai nosi sw=4 ts=4 expandtab
