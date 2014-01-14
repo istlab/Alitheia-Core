38c85426ac85868b57eb9324f4f9e84df76acd5c
qstokkink <goqs@hotmail.com>
2014-01-14 12:53:52 +0100

    Added test stubs

---
+++ alitheia/core/src/test/java/eu/sqooss/impl/service/updater/test/OhlohUpdaterTest.java
@@ -0,0 +1,25 @@
+package eu.sqooss.impl.service.updater.test;
+
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class OhlohUpdaterTest {
+
+	@Test
+    public void stub() {
+
+    }
+	
+	@Before
+    public void setUp() {
+
+    }
+ 
+    @After
+    public void tearDown() {
+
+    }
+	
+}
