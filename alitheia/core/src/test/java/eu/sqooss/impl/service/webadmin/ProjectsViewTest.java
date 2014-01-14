815fc88239d83e838d5997e0c0fa0128c1c5777c
rmhartog <r.m.hartog@hotmail.com>
2014-01-14 10:16:27 +0100

    Added hamcrest xml-matchers. Used in ProjectsViewTest to verify HTML.

--- alitheia/core/src/test/java/eu/sqooss/impl/service/webadmin/ProjectsViewTest.java
+++ alitheia/core/src/test/java/eu/sqooss/impl/service/webadmin/ProjectsViewTest.java
@@ -1,9 +1,15 @@
 package eu.sqooss.impl.service.webadmin;
 
-import static org.hamcrest.xml.HasXPath.hasXPath;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.xmlmatchers.transform.XmlConverters.the;
+import static org.xmlmatchers.xpath.HasXPath.hasXPath;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.containsString;
+
+import org.hamcrest.Description;
+import org.hamcrest.BaseMatcher;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -19,22 +25,11 @@ import javax.xml.xpath.XPathExpressionException;
 
 import org.junit.Test;
 import org.w3c.dom.Document;
-import org.xml.sax.EntityResolver;
-import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 import eu.sqooss.service.pa.PluginInfo;
 
 public class ProjectsViewTest {
-	protected Document parse(String input) throws ParserConfigurationException,
-			SAXException, IOException {
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder builder = factory.newDocumentBuilder();
-		String xml = "<root>" + input + "</root>";
-		Document document = builder.parse(new ByteArrayInputStream(xml.getBytes()));
-		return document;
-	}
-
 	@Test
 	public void shouldShowNoLastAppliedVersionForEmptyList() {
 		StringBuilder html = new StringBuilder();
@@ -56,12 +51,13 @@ public class ProjectsViewTest {
 
 		assertEquals("", html.toString());
 	}
-
+	
 	@Test
 	public void shouldShowInstalledPlugins()
 			throws ParserConfigurationException, SAXException, IOException,
 			XPathExpressionException {
-		StringBuilder html = new StringBuilder();
+		// Arrange
+		StringBuilder builder = new StringBuilder();
 		String hash1 = "p1hash";
 		String name1 = "p1 plugin";
 		String hash2 = "p2hash";
@@ -77,34 +73,31 @@ public class ProjectsViewTest {
 		p2.setPluginName(name2);
 		Collection<PluginInfo> metrics = Arrays.asList(p1, p2);
 
-		ProjectsView.showLastAppliedVersion(null, metrics, html);
+		// Act
+		ProjectsView.showLastAppliedVersion(null, metrics, builder);
 
-		String submit = Pattern.quote(ProjectsView.SUBMIT);
-
-		String sethash1 = "javascript:document.getElementById\\('"
-				+ ProjectsView.REQ_PAR_SYNC_PLUGIN + "'\\).value='" + hash1
-				+ "';";
-		String onclick1 = "onclick\\s*=\\s*\"" + sethash1 + submit + "\"";
-		String input1 = "<input\\s+(.*\\s+)" + onclick1 + "(.*)>\\s*";
-		String column1 = "<td(.*)>\\s*" + input1 + "&nbsp;" + name1 + "</td>";
-		String expected1 = "<tr>\\s*" + column1 + "\\s*</tr>\\s*";
-
-		String sethash2 = "javascript:document.getElementById\\('"
-				+ ProjectsView.REQ_PAR_SYNC_PLUGIN + "'\\).value='" + hash2
-				+ "';";
-		String onclick2 = "onclick\\s*=\\s*\"" + sethash2 + submit + "\"";
-		String input2 = "<input\\s+(.*\\s+)" + onclick2 + "(.*)>\\s*";
-		String column2 = "<td(.*)>\\s*" + input2 + "&nbsp;" + name2 + "</td>";
-		String expected2 = "<tr>\\s*" + column2 + "\\s*</tr>\\s*";
-
-		assertTrue(html.toString().matches(expected1 + expected2));
+		// Assert
+		String html = "<root>" + builder.toString().replace("&nbsp;", " ") + "</root>";
 		
-//		RENG: Replace the regular expressions with XPath expressions.		
-//		
-//		String xml = html.toString();
-//		xml = xml.replace("&nbsp;", " ");
-//		
-//		assertThat(parse(xml), hasXPath("/root"));
+		String onclick1 = "javascript:document.getElementById('" + ProjectsView.REQ_PAR_SYNC_PLUGIN + "').value='" + hash1 +"';" + ProjectsView.SUBMIT;
+		String onclick2 = "javascript:document.getElementById('" + ProjectsView.REQ_PAR_SYNC_PLUGIN + "').value='" + hash2 +"';" + ProjectsView.SUBMIT;
+
+		// there must be two rows.
+		assertThat(the(html), hasXPath("count(/root/tr)", equalTo("2")));
+		// the first row has one column.
+		
+		assertThat(the(html), hasXPath("count(/root/tr[1]/td)", equalTo("1")));
+		// that column has an input of type button that does 'onclick1' on click.
+		assertThat(the(html), hasXPath("/root/tr[1]/td/input[@type='button']/@onclick", equalTo(onclick1)));
+		// that column's text should contain the plugin name
+		assertThat(the(html), hasXPath("string-join(/root/tr[1]/td/text(), '')", containsString(name1)));
+		
+		// the second row has one column.
+		assertThat(the(html), hasXPath("count(/root/tr[2]/td)", equalTo("1")));
+		// that column has an input of type button that does 'onclick2' on click.
+		assertThat(the(html), hasXPath("/root/tr[2]/td/input[@type='button']/@onclick", equalTo(onclick2)));
+		// that column's text should contain the plugin name
+		assertThat(the(html), hasXPath("string-join(/root/tr[2]/td/text(), '')", containsString(name2)));
 	}
 
 	@Test
