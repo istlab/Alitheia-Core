142ea90fa8c9f8085e6feac93c8c5441997c52c0
qstokkink <goqs@hotmail.com>
2014-01-15 09:23:22 +0100

    Finished OhlohUpdater tests

--- alitheia/core/src/test/java/eu/sqooss/impl/service/updater/test/OhlohUpdaterTest.java
+++ alitheia/core/src/test/java/eu/sqooss/impl/service/updater/test/OhlohUpdaterTest.java
@@ -12,6 +12,7 @@ import org.mockito.Mockito;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Date;
 
 import eu.sqooss.service.db.DAObject;
 import eu.sqooss.service.db.DBService;
@@ -30,18 +31,43 @@ public class OhlohUpdaterTest {
 	
 	@Test
 	/**
+	 * Invoke run() normally, with a known developer
+	 */
+    public void testRunKnown() throws Exception {
+		// -- Given
+		OhlohDeveloper od = Mockito.mock(OhlohDeveloper.class);
+		Mockito.when(OhlohDeveloper.getByOhlohId(anyString())).thenReturn(od).thenReturn(null);
+		
+		// -- When
+		ohlohupdater.run();
+		
+		// -- Then
+		verify(od).setEmailHash("L9ThxnotKPzthJ7hu3bnORuT6xI=");
+		verify(od).setTimestamp(Mockito.any(Date.class));
+		verify(od).setUname("TestDeveloper0");
+    }
+	
+	@Test
+	/**
 	 * Invoke run() normally 
 	 */
     public void testRun() throws Exception {
+		// -- Given
 		Mockito.when(OhlohDeveloper.getByOhlohId(anyString())).thenReturn(null);
-		ohlohupdater.run();
-		
 		DBService dbs = ohlohupdater.getMockedDBService();
 		
+		// -- When
+		ohlohupdater.run();
+		
+		// -- Then
 		//file0.xml contains 0 entries
 		//file1.xml contains 1 entry
 		//file2.xml contains 3 entries
 		verify(dbs, Mockito.times(4)).addRecord((DAObject) anyObject());
+		
+		//file0.xml has no entries
+		//file3.xml contains an xml error
+		verify(mockedLogger, Mockito.times(2)).warn(anyString());
     }
 	
 	@Test(expected = FileNotFoundException.class) 
@@ -50,18 +76,24 @@ public class OhlohUpdaterTest {
 	 * the path does not exist
 	 */
     public void testRunInvalidPath() throws Exception {
+		// -- Given
+		//A non-existent Ohloh path
 		System.setProperty("eu.sqooss.updater.ohloh.path", "THISpathDOESnotEXIST");
-		
 		OhlohUpdaterWrapper updater = new OhlohUpdaterWrapper();
 		updater.setUpdateParams(Mockito.mock(StoredProject.class), mockedLogger);
-		
+		//RuntimeException is not equal to FileNotFoundException, therefore the test will fail
+		//  if a FileNotFoundException is not thrown
 		Exception e = new RuntimeException();
 		try {
+			// -- When
 			updater.run();
 		} catch (Exception ee){
 			e = ee;
 		}
 		
+		// -- Then
+		//Note that we have to save the thrown error and throw it ourselves later: to be
+		//  able to veryify the logger error entry
 		verify(mockedLogger).error(anyString());
 		throw e;
     }
@@ -72,18 +104,24 @@ public class OhlohUpdaterTest {
 	 * the path is null
 	 */
     public void testRunNullPath() throws Exception {
+		// -- Given
+		//No Ohloh path set
 		System.clearProperty("eu.sqooss.updater.ohloh.path");
-		
 		OhlohUpdaterWrapper updater = new OhlohUpdaterWrapper();
 		updater.setUpdateParams(Mockito.mock(StoredProject.class), mockedLogger);
-		
+		//RuntimeException is not equal to FileNotFoundException, therefore the test will fail
+		//  if a FileNotFoundException is not thrown
 		Exception e = new RuntimeException();
 		try {
+			// -- When
 			updater.run();
 		} catch (Exception ee){
 			e = ee;
 		}
 		
+		// -- Then
+		//Note that we have to save the thrown error and throw it ourselves later: to be
+		//  able to veryify the logger error entry
 		verify(mockedLogger).error(anyString());
 		throw e;
     }
@@ -93,8 +131,9 @@ public class OhlohUpdaterTest {
 	 * priority() should return 3
 	 */
     public void testPriority() {
+		// -- When
 		long priority = ohlohupdater.priority();
-		
+		// -- Then
 		assertEquals(3, priority);
     }
 	
@@ -103,24 +142,24 @@ public class OhlohUpdaterTest {
 	 * getJob() should return the OhlohUpdater itself
 	 */
     public void testGetJob() {
+		// -- When
 		Job job = ohlohupdater.getJob();
-		
+		// -- Then
 		assertEquals(ohlohupdater, job);
     }
 	
 	@Before
     public void setUp() {
+		//Divert the Ohloh path to our local test resource folder
 		File f = new File("src" + File.separator + "test" + File.separator + "resources" + File.separator + "ohlohpath");
-		
 		System.setProperty("eu.sqooss.updater.ohloh.path", f.getAbsolutePath());
+		//Wrap the OhlohUpdater class, so we can modify protected variables (and hereby facilitate testing)
 		ohlohupdater = new OhlohUpdaterWrapper();
-		
+		//Mock and store the logger so we can verify error() and warning() logging
 		mockedLogger = Mockito.mock(Logger.class);
-		
 		ohlohupdater.setUpdateParams(Mockito.mock(StoredProject.class), mockedLogger);
-		
+		//Mock OhlohDeveloper static methods so we can intercept calls to getByOhlohId()
 		mockStatic(OhlohDeveloper.class);
-
     }
  
     @After
